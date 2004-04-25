4056f8da546cbe55bcc97bb2f3c39efb94223077
Piotr Kupisiewicz <deletek@ekg2.org>
2004-04-25 10:05:27 +0000

    Sprawdzanie czy wszystko po kolei wykonało się poprawnie Dodanie jeszcze jednego wpisu aclocal - w niektórych systemach konieczne (del)

--- autogen.sh
+++ autogen.sh
@@ -6,21 +6,23 @@ else
 	test -f config.log && ARGS=`grep '^  \$ \./configure ' config.log | sed 's/^  \$ \.\/configure //' 2> /dev/null`
 fi
 
+echo "Running aclocal..."
+aclocal -I m4 || exit 1
+
 echo "Running autoheader..."
-autoheader
+autoheader || exit 1 
 
 echo "Running libtoolize..."
-libtoolize --ltdl
+libtoolize --ltdl || exit 1 
 
 echo "Running automake..."
-automake --foreign --add-missing
+automake --foreign --add-missing || exit 1 
 
-echo "Running aclocal..."
-aclocal -I m4
+echo "Running once again aclocal..."
+aclocal -I m4 || exit 1 
 
 echo "Running autoconf..."
-autoconf
+autoconf || exit 1
 
 test x$NOCONFIGURE = x && echo "Running ./configure $ARGS" && ./configure $ARGS
-
 
