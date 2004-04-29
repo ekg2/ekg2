a5c70a6c51a246fa701250d83cd9cb005cb77b63
Maciej Pietrzak <mpietrzak@gmail.com>
2004-04-29 11:42:14 +0000

    kolejnosc w autogen.sh i AC_DISABLE_STATIC w configure.ac

--- autogen.sh
+++ autogen.sh
@@ -6,20 +6,17 @@ else
 	test -f config.log && ARGS=`grep '^  \$ \./configure ' config.log | sed 's/^  \$ \.\/configure //' 2> /dev/null`
 fi
 
+echo "Running libtoolize..."
+libtoolize --ltdl || exit 1 
+
 echo "Running aclocal..."
-aclocal -I m4 || exit 1
+aclocal -I m4 || exit 1 
 
 echo "Running autoheader..."
 autoheader || exit 1 
 
-echo "Running libtoolize..."
-libtoolize --ltdl || exit 1 
-
 echo "Running automake..."
-automake --foreign --add-missing 
-
-echo "Running once again aclocal..."
-aclocal -I m4 || exit 1 
+automake --foreign --add-missing  || exit 1
 
 echo "Running autoconf..."
 autoconf || exit 1
