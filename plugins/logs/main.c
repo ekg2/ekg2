2a6f0cdeaf149090d859b5f9972b52dd390a91d7
Adam Kuczyński <dredzik@dredzik.pl>
2005-01-05 12:12:25 +0000

    [logs] Added xml support, needs to be checked by someone.

--- plugins/logs/main.c
+++ plugins/logs/main.c
@@ -1,8 +1,9 @@
 /* $Id$ */
 
 /*
- *  (C) Copyright 2003-2004 Tomasz Torcz <zdzichu@irc.pl>
+ *  (C) Copyright 2003-2005 Tomasz Torcz <zdzichu@irc.pl>
  *                          Leszek Krupiñski <leafnode@wafel.com>
+ *                          Adam Kuczyñski <dredzik@ekg2.org>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License Version
@@ -209,6 +210,75 @@ FILE* logs_open_file(char *path, char *ext, int makedir)
 	return fdesc;
 };
 
+/*
+ * otwarcie pliku do zapisu/odczytu - wersja dla xml
+ * tworzy wszystkie katalogi po drodze, je¶li nie istniej± i mkdir =1
+ * ext - rozszerzenie jakie nadac
+ * zwraca numer deskryptora b±d¼ NULL
+ */
+
+FILE* logs_open_xml_file(char *path, char *ext, int makedir)
+{
+	struct stat statbuf;
+	char *dir, *fullname, *slash;
+	int slash_pos = 0;
+	long pos = 0;
+	FILE* fdesc;
+
+        debug("[logs] opening xml log file\n");
+
+	while (makedir) {
+		if (!(slash = xstrchr(path + slash_pos, '/'))) {
+			// nie ma juz slashy - zostala tylko nazwa pliku
+			makedir = 0; // konczymy petle
+			continue;
+		};
+
+		slash_pos = slash - path + 1;
+		dir = xstrndup(path, slash_pos);
+
+		if (stat(dir, &statbuf) != 0 && mkdir(dir, 0700) == -1) {
+			char *bo = saprintf("nie mo¿na %s bo %s", dir, strerror(errno));
+			print("generic",bo); // XXX usun±æ !! 
+			xfree(bo);
+			xfree(dir);
+			return NULL;
+		}
+		xfree(dir);
+	} // while mkdir
+
+	if (ext)
+		fullname = saprintf("%s.%s", path, ext);
+	else
+		fullname = xstrdup(path);
+
+	fdesc = fopen(fullname, "r");
+	
+	/*
+	 * if the file does not exist create it and 
+	 * put some xml specific things there
+	 */
+	if (fdesc == NULL) {
+		fdesc = fopen(fullname, "a+");
+		fputs("<?xml version=\"1.0\"?>\n", fdesc);
+		fputs("<!DOCTYPE ekg2log PUBLIC \"-//ekg2log//DTD ekg2log 1.0//EN\" ", fdesc);
+		fputs("\"http://www.ekg2.org/DTD/ekg2log.dtd\">\n", fdesc);
+		fputs("<ekg2log xmlns=\"http://www.ekg2.org/DTD/\">\n", fdesc);
+		fclose(fdesc);
+	} else {
+		fseek(fdesc, 0, SEEK_END);
+		pos = ftell(fdesc);
+		fclose(fdesc);
+		truncate(fullname, pos - 11); //very ugly - 11 is charcount of </ekg2log>\n
+	}
+	
+	fdesc = fopen(fullname, "a+");
+	xfree(fullname);
+
+	return fdesc;
+};
+
+
 /**
  * przygotowuje timstamp do wstawienia do logów
  */
@@ -262,9 +332,14 @@ void logs_handler(void *data, va_list ap)
 	debug("[logs] logging to file %s\n", path);
 
 	if (config_logs_log == 1 && xstrstr(log_formats, "simple"))
-			logs_simple(path, session,
-				((class == EKG_MSGCLASS_SENT || class == EKG_MSGCLASS_SENT_CHAT) ? rcpts[0] : uid),
-				 text, sent, class, seq);
+		logs_simple(path, session, 
+			((class == EKG_MSGCLASS_SENT || class == EKG_MSGCLASS_SENT_CHAT) ? rcpts[0] : uid), 
+			text, sent, class, seq);
+	else if (config_logs_log == 2 && xstrstr(log_formats, "xml"))
+		logs_xml(path, session, 
+			((class == EKG_MSGCLASS_SENT || class == EKG_MSGCLASS_SENT_CHAT) ? rcpts[0] : uid), 
+			text, sent, class, seq);
+	
 
 	// itd. dla innych formatow logow
 
@@ -366,8 +441,76 @@ void logs_simple(char *path, char *session, char *uid, char *text, time_t sent, 
  * zapis w formacie xml
  */
 
-void logs_xml()
+void logs_xml(char *path, char *session, char *uid, char *text, time_t sent, int class, int seq)
 {
+	FILE *file;
+	char *textcopy = log_escape(text);
+	char *timestamp = prepare_timestamp((time_t)time(0));
+	char *senttimestamp = prepare_timestamp(sent);
+	session_t *s = session_find((const char*)session);
+	char *gotten_uid = get_uid(s, uid);
+	char *gotten_nickname = get_nickname(s, uid);
+
+	if ( gotten_uid == NULL )
+		gotten_uid = uid;
+
+	if ( gotten_nickname == NULL )
+		gotten_nickname = uid;
+
+	if (!(file = logs_open_xml_file(path, "xml", 1)) || !s) {
+		xfree(senttimestamp);
+		xfree(timestamp);
+		xfree(textcopy);
+		return ;
+	}
+
+	fputs("  <message class=\"",file);
+		
+	switch ((enum msgclass_t)class) {
+		case EKG_MSGCLASS_MESSAGE	: fputs("msgrecv", file);
+						  break;
+		case EKG_MSGCLASS_CHAT		: fputs("chatrecv", file);
+						  break;
+		case EKG_MSGCLASS_SENT		: fputs("msgsend", file);
+						  break;
+		case EKG_MSGCLASS_SENT_CHAT	: fputs("chatsend", file);
+						  break;
+		case EKG_MSGCLASS_SYSTEM	: fputs("msgsystem", file);
+						  break;
+		default				: fputs("chatrecv", file);
+						  break;
+
+	};
+	
+	fputc('"', file);
+
+	/*
+	 * <message="chatsend" uid="&numer;" nick="&nick;" timestamp="&czas;">&tre¶æ</message>
+	 * <message="chatrecv" uid="&numer;" nick="&nick;" timestamp="&czas_otrzymania;" 
+	 * senttimestamp="&czas_nadania">&tre¶æ</message>
+	 */
+
+	fputs(" uid=\"", file);	fputs(gotten_uid, file);	fputc('"', file);
+	fputs(" nick=\"", file);	fputs(gotten_nickname, file);	fputc('"', file);
+
+	fputs(" timestamp=\"", file);	fputs(timestamp, file);	fputc('"', file);
+
+	if (class == EKG_MSGCLASS_MESSAGE || class == EKG_MSGCLASS_CHAT) {
+		fputs(" senttimestamp=\"", file);
+		fputs(senttimestamp, file);
+		fputc('"', file);
+	}
+
+	fputc('>', file);
+	
+	fputs(textcopy, file);
+	fputs("</message>\n", file);
+	fputs("</ekg2log>\n", file);
+
+	xfree(senttimestamp);
+	xfree(timestamp);
+	xfree(textcopy);
+	fclose(file);
 };
 
 /*
