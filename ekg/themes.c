ae24b285c91b71ed5417bca94f4f0261516680db
Piotr Kupisiewicz <deletek@ekg2.org>
2005-01-27 14:05:04 +0000

    auto_away/auto_back problem fixed (fucked up by zdzichu:))

--- ekg/themes.c
+++ ekg/themes.c
@@ -1041,9 +1041,9 @@ void theme_init()
         format_add("descr_too_long", _("%! Description longer than maximum %T%1%n characters\nDescr: %B%2%b%3%n\n"), 1);
 
         format_add("auto_away", _("%> (%1) Auto %Gaway%n\n"), 1);
-        format_add("auto_away_descr", _("%> (%2) Auto %Gaway%n: %T%1%n\n"), 1);
+        format_add("auto_away_descr", _("%> (%3) Auto %Gaway%n: %T%1%n%2%n\n"), 1);
         format_add("auto_back", _("%> (%1) Auto back - %Yavailable%n\n"), 1);
-        format_add("auto_back_descr", _("%> (%2) Auto back - %Yavailable%n: %T%1%n\n"), 1);
+        format_add("auto_back_descr", _("%> (%3) Auto back - %Yavailable%n: %T%1%n%2%n\n"), 1);
 
         /* pomoc */
         format_add("help", "%> %T%1%n%2 - %3\n", 1);
