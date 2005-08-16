d17deca70bb0cb408e17ccef6978ac5a9d2991c2
Leszek Krupiński <leafnode@gmail.com>
2005-08-16 14:54:16 +0000

    - updated [dj]

--- ekg/scripts.c
+++ ekg/scripts.c
@@ -5,15 +5,25 @@
 #include <errno.h>
 #include <string.h>
 
-
+#include <ekg/stuff.h>
 #include <ekg/windows.h>
 #include <ekg/scripts.h>
 #include <ekg/xmalloc.h>
 
-COMMAND(script_command_handlers);
+/* TODO && BUGS 
+ * - cleanup kodu!
+ * - multiple handler for commands && var_changed.
+ * - usuwanie listy list_remove()
+ * - wszystkie memleaki won!
+ */
 
-/* scriptlang main func */
-        
+COMMAND(script_command_handlers);
+int script_timer_handlers(int type, void *d);
+void script_var_changed(const char *var);
+int script_query_handlers(void *data, va_list ap);
+
+/********************************************************************************** scriptlang */
+
 scriptlang_t *scriptlang_from_ext(char *ext)
 {
 	list_t l;
@@ -37,17 +47,15 @@ static int script_register_compare(void *data1, void *data2)
 	return scr1->prio - scr2->prio;
 }
 
-
 int scriptlang_register(scriptlang_t *s, int prio)
 {
 	s->prio = prio;
 	list_add_sorted(&scriptlang, s, 0, script_register_compare);
 
 	s->init();
-	
+
 	return 0;
 }
-
 
 int scriptlang_unregister(scriptlang_t *s)
 {
@@ -59,7 +67,47 @@ int scriptlang_unregister(scriptlang_t *s)
 	return 0;
 }
 
-/* scripts func */
+/**************************************************************************************/
+
+int script_list(scriptlang_t *s)
+{
+	list_t l;
+	script_t *scr;
+	scriptlang_t *lang;
+	int i = 0;
+	
+	for (l = scripts; l; l = l->next) {
+		scr = l->data;
+		lang = scr->lang;
+		if (!s || scr->lang == s) {
+			print("script_list", scr->name, scr->path, lang->name);
+			i++;
+		}
+	
+	}
+	if (!i)
+		print("script_list_empty");
+	return i;
+}
+
+int script_var_list(script_t *scr)
+{
+	list_t l;
+	int i = 0;
+        for (l = script_vars; l; l = l->next) {
+		script_var_t *v = l->data;
+                if (!scr || v->scr == scr) {
+			print("script_varlist", v->name, v->value, v->private);
+			i++;
+		}
+        }
+	if (!i)
+		print("script_varlist_empty");
+
+        return i;
+}
+
+/*****************    SCRIPTS MANGMENT *********************************************/
 
 char *script_find_path(char *name) {
 	FILE *fajl;
@@ -116,10 +164,32 @@ char *script_find_path(char *name) {
 
 int script_unload(script_t *scr)
 {
+	list_t l, prev;
+	
+	script_command_t *c;
+	script_timer_t   *t;
+	script_var_t     *v;
+	script_query_t   *q;
+	
 	scriptlang_t *slang = scr->lang;
+	
+/* przeszukac liste timerow i komand, jak cos to je wywalic */
+
+	for (prev = l = script_timers; l; l = l->next, prev = l) { t = l->data; if (!t) continue;
+                if (t->scr == scr) { script_timer_unbind(t, 1);  l = prev; } }
+
+	for (prev = l = script_commands_bindings; l; l = l->next, prev = l) { c = l->data; if (!c) continue;
+                if (c->scr == scr) { script_command_unbind(c, 1);  l = prev; } }
+
+	for (prev = l = script_vars; l; l = l->next, prev = l) { v = l->data; if (!v) continue;
+                if (v->scr == scr) { v->scr = NULL; v->private = NULL; /* script_var_unbind(v, 1);   l = prev; */ } ; }
+
+	for (prev = l = script_queries; l; l = l->next, prev = l) { q = l->data; if (!q) continue;
+                if (q->scr == scr) { script_query_unbind(q, 1); l = prev; } }
+		
 	if (slang->script_unload(scr))
 		return -1;
-	
+
 	print("script_removed", scr->name);
 	
 	xfree(scr->name);
@@ -164,16 +234,19 @@ int script_unload_lang(scriptlang_t *s)
 	scriptlang_t *lang;
 	script_t *scr;
 	list_t l;
-	list_t prev = scripts;	
-	for (l = scripts; l;) {
+	list_t prev;
+
+	for (prev = l = scripts; l; l = l->next, prev = l) {
 		scr = l->data;
+		if (!scr) 
+			continue;
+		
 		lang = scr->lang;
 		if (!s || scr->lang == s) {
 			script_unload(scr);
 			l = prev;
 		}
-		prev = l;
-		l = l->next;
+		
 	}
 	return 0;
 
@@ -187,6 +260,7 @@ int script_load(scriptlang_t *s, char *name)
 	char 		*path;
 	struct stat 	st;
 	char           *name2;
+	int 		ret;
 	
 	if (s && !xrindex(name, '.')) {
 // TODO dodac do name rozszerzenie `s->ext`
@@ -222,30 +296,30 @@ int script_load(scriptlang_t *s, char *name)
 			script_unload(script_find(slang, name2));
 		}
 
-
 		scr = xmalloc(sizeof(script_t));
 		scr->path = xstrdup(path);
 		scr->name = name2;
 		
-
 		list_add(&scripts, scr, 0); /* to powinno byc przed script_loaded... nie fajne. */
 		
 		scr->lang = slang;
-		scr->mask = slang->script_load(scr);
+		ret = slang->script_load(scr);
 
-		debug("[script] sc_mask == %d\n", scr->mask);
-		if (scr->mask < 1) {
-			if (scr->mask == -1)
+		debug("[script] ret == %d\n", ret);
+		
+		if (ret < 1) {
+			if (ret == -1)
 				print("generic_error", "@script_load error");
-			else if (scr->mask == 0)
+			else if (ret == 0)
 				print("generic_error", "@script_load script has no handler or error in getting handlers.");
+			script_unload(scr);
 			
-			xfree(scr->path);
-			xfree(scr->name);
-			xfree(scr);
-			
+//			xfree(scr->path);
+//			xfree(scr->name);
+//			xfree(scr);
+//    			list_remove(&scripts, scr, 0);			
+
 			xfree(path);
-			list_remove(&scripts, scr, 0);
 			
 			return -1;
 		
@@ -260,28 +334,97 @@ int script_load(scriptlang_t *s, char *name)
 	return 0;
 }
 
-int script_list(scriptlang_t *s)
+
+/******************************* VARIABLES ****************************/
+
+
+script_var_t *script_var_find(script_t *scr, const char *name)
 {
 	list_t l;
-	script_t *scr;
-	scriptlang_t *lang;
-	int i = 0;
-	
-	for (l = scripts; l; l = l->next) {
-		scr = l->data;
-		lang = scr->lang;
-		if (!s || scr->lang == s) {
-			print("script_list", scr->name, scr->path, lang->name);
-			i++;
+        for (l = script_vars; l; l = l->next) {
+		script_var_t *v = l->data;
+                if ( /*  (!scr || v->scr == scr) && */ !xstrcasecmp(v->name, name)) {
+			return v;
 		}
-	
-	}
-	if (!i)
-		print("script_list_empty");
-	return i;
+        }
+        return NULL;
 }
 
-int script_command_unbind(script_command_t *scr_comm, int free)
+
+
+script_var_t *script_var_add(scriptlang_t *s, script_t *scr, char *name, char *value, void *handler)
+{
+	script_var_t *tmp;
+	tmp = script_var_find(scr, name);
+	debug("[script_variable_add] (%s = %s) scr=%x tmp=%x\n", name, value, scr, tmp);
+
+	if (tmp) {
+		tmp->scr = scr;
+		tmp->private = handler;
+		variable_set(name, value, 0);
+	}
+	else if (!tmp) {
+		SCRIPT_BIND_HEADER(script_var_t);
+	
+		temp->name  = name;
+		temp->value = value;
+		variable_add(NULL, temp->name, VAR_STR, 1, &(temp->value), &script_var_changed, NULL, NULL);
+
+		SCRIPT_BIND_FOOTER(script_vars);
+	} 
+	
+	return tmp;
+}
+
+
+int script_variables_read() {
+// czytamy z scripts-var
+	FILE *f;
+        char *line;
+
+	if (!(f = fopen(prepare_path("scripts-var", 0), "r"))) {
+		debug("Error opening script variable file..\n");
+		return -1;
+	}
+	
+        while ((line = read_file(f))) {
+		script_var_add(NULL, NULL, xstrdup(line), NULL, NULL);
+                xfree(line);
+        }
+
+        fclose(f);
+	return 0;
+}
+
+
+int script_variables_free(int free) {
+	FILE *f = fopen(prepare_path("scripts-var", 0), "w");
+	list_t l;
+	debug("[script_variables_free()]%s saveing vars...\n", (free) ? " freeing &&" : "");
+
+        for (l = script_vars; l; l = l->next) {
+		script_var_t *v = l->data;
+		
+		if (f)
+			fprintf(f, "%s\n", v->name);
+		if (free) {
+			xfree(v->name);
+//			xfree(v->value); // zwolnione juz przez variables_free()
+			xfree(v->private);
+			xfree(v);
+		}
+        }
+	fclose(f);
+	return 0;
+}
+
+int script_variables_write() {
+	return script_variables_free(0);
+}
+
+/************** COMMANDS **********************************************************/
+
+int script_command_unbind(script_command_t *scr_comm, int from)
 {
 	int notfound = 1;
 // TODO: powinnismy sprawdzic czy to polecenie wystepuje w innych handlerach.., jedno polecenie bedzie moze byc podbindowane pod wiele handlerow !
@@ -289,64 +432,139 @@ int script_command_unbind(script_command_t *scr_comm, int free)
 	if (notfound)
 		command_remove(NULL, scr_comm->comm);
 
-	if (free) {
+	if (from) {
 		xfree(scr_comm->private);
 	}
 	xfree(scr_comm->comm);
 	xfree(scr_comm);
-	
 	
 	list_remove(&script_commands_bindings, scr_comm, 0);
 	return notfound;
 	
 }
 
-int script_command_bind(scriptlang_t *s, script_t *scr, char *command, void *handler) 
+script_command_t *script_command_bind(scriptlang_t *s, script_t *scr, char *command, void *handler) 
 {
-	script_command_t *scrcomm;
-	debug("[script] (bind command) 0x%x 0x%x %s 0x%x\n", s, scr, command, handler);
-// TODO: sprawdzic czy wartosci sa ok.
+	SCRIPT_BIND_HEADER(script_command_t);
+	
+	temp->comm = command;
+	command_add(NULL, temp->comm, "?", script_command_handlers, 0, NULL);
+	
+	SCRIPT_BIND_FOOTER(script_commands_bindings);
+}
 
-	scrcomm = xmalloc(sizeof(script_command_t));
+/************************************* QUERIES ***********************************************/
+int script_query_unbind(script_query_t *squery, int from)
+{
+	scriptlang_t *slang = squery->scr->lang;
+// TODO: tak jak w command_unbind jeden handler moze byc pod wiele skryptow.
+	query_disconnect(slang->plugin, squery->query_name);
+//	query_disconnect(NULL, squery->query_name);
+	list_remove(&script_queries, squery, 0);
+}
+
+script_query_t *script_query_bind(scriptlang_t *s, script_t *scr, char *query_name, void *handler)
+{
+	SCRIPT_BIND_HEADER(script_query_t);
 	
-	scrcomm->scr = scr;
-	scrcomm->comm = command;
-	scrcomm->private = handler;
+	temp->query_name	= query_name;
 	
-	list_add(&script_commands_bindings, scrcomm, 0);
+// argc i argv_type uzupelnic... z czego ? xstrcmp() ? 
+
+#define CHECK(x) if (!xstrcmp(query_name, x)) 
+#define ARG(x, y) temp->argv_type[x] = y;
+
+	CHECK("protocol-disconnected")   { temp->argc = 1; ARG(0, SCR_ARG_CHARP); }
+	else CHECK("protocol-connected") { temp->argc = 1; ARG(0, SCR_ARG_CHARP); }
+	else CHECK("protocol-status")    { temp->argc = 4; ARG(0, SCR_ARG_CHARP); ARG(1, SCR_ARG_CHARP); ARG(2, SCR_ARG_CHARP); ARG(3, SCR_ARG_CHARP); }
+	else CHECK("protocol-message")   { temp->argc = 7; ARG(0, SCR_ARG_CHARP);
+							   ARG(1, SCR_ARG_CHARP);
+							   ARG(2, SCR_ARG_CHARPP);
+							   ARG(3, SCR_ARG_CHARP);
+//							   ARG(4, SCR_ARG_UNITPP);
+							   ARG(5, SCR_ARG_INT); // time_t
+							   ARG(6, SCR_ARG_INT); 
+					 }
+	else CHECK("ui-keypress")	 { temp->argc = 1; ARG(0, SCR_ARG_INT); }
+	else                             { temp->argc = 0; }
+
+#undef CHECK
+#undef ARG	
+
+//	query_connect(NULL, temp->query_name, script_query_handlers, temp);
+// TOD: jesli handler dla tego pluginu nie istnieje to dodac.
+	query_connect(s->plugin, temp->query_name, script_query_handlers, temp);
+
+
+	SCRIPT_BIND_FOOTER(script_queries);
+}
+
+/************************************ TIMERS ************************************************/
+
+
+int script_timer_unbind(script_timer_t *stimer, int from)
+{
+// stimer->removed -> 1 - w trakcie usuwania ; 2 -> blad skryptu i usuwanie || usunieto 3 -> usunieto
+
+//	debug("[script_timer_unbind] %d %d\n", stimer, from );
+	if (stimer->removed > 2) { /* na wszelki wypadek */
+		debug("[script_ierror] stimer->removed = %d\n", stimer->removed);
+		return 0;
+	}
+	if (!stimer->removed) stimer->removed = 1;
 	
-	command_add(NULL, command, "?", script_command_handlers, 0, NULL);
+	script_timer_handlers(1, stimer);
+	if (from) 
+		timer_remove(NULL, stimer->self->name);
+	xfree(stimer->private);
+	xfree(stimer->name);
+//	xfree(stimer); // timer_remove() za nas to zwalnia.
+	stimer->removed = 3; /* na wszelki wypadek */
+	
+	list_remove(&script_timers, stimer, 0);
 	return 0;
 }
-/* HANDLERS */
+
+script_timer_t *script_timer_bind(scriptlang_t *s, script_t *scr, int freq, void *handler)
+{
+	SCRIPT_BIND_HEADER(script_timer_t);
+	
+	temp->name    = saprintf("scr_%x", (int) temp); /* truly unique ;p */
+	temp->self    = timer_add(NULL, (const char *) temp->name, freq, 1, &script_timer_handlers, (void *) temp);
+	
+	SCRIPT_BIND_FOOTER(script_timers);
+} 
+
+/******************************************************************** HANDLERY DO SKRYPTOW */
+
+void script_var_changed(const char *var) {
+	script_var_t     *temp = script_var_find(NULL, var);
+	SCRIPT_HANDLER_HEADER(script_handler_var_t);
+	debug("[script_variable_changed] varname = %s newvalue = %s\n", var, temp->value);
+	SCRIPT_HANDLER_FOOTER(script_handler_var, temp->value);
+	
+	return;
+}
 
 COMMAND(script_command_handlers)
 {
-	script_command_t *scr_comm;
-	script_t 	 *scr;
-	scriptlang_t 	 *slang;
+	script_command_t *temp;
+	SCRIPT_HANDLER_HEADER(script_handler_command_t);
+	
 	list_t l;
 	int i = 0;
 	
-	int ret = 0;
-	
 	debug("[script_command_handlers] %s %s\n", name, params[0]);
-	// w name
+
 	for (l = script_commands_bindings; l; l = l->next) {
-		scr_comm = l->data;
+		temp = l->data;
 		
-		if (!xstrcmp(name, scr_comm->comm)) {
+		if (!xstrcmp(name, temp->comm)) {
 			debug("[script_command_handlers] found handler for %s\n", name);
-			scr	 = scr_comm->scr;
-			slang	 = scr->lang;
 			
-			ret = slang->script_handler_command(scr, scr_comm, (char **) params);
-			if (ret == SCRIPT_COMMAND_DELETE) {
-				debug("[script_command_handlers] script or scriptlang want to delete this handler\n");
-				if (script_command_unbind(scr_comm, 1))
-					return ret;
-				return 0; 
-// TODO: powinnismy leciec z lista dalej....
+			SCRIPT_HANDLER_FOOTER(script_handler_command, (char **) params)	{
+				if (script_command_unbind(temp, 1))
+					return ret; // TODO: powinnismy leciec z lista dalej....
 			}
 			i++;
 		}
@@ -361,55 +579,48 @@ COMMAND(script_command_handlers)
 
 }
 
-int script_protocol_message(void *data, va_list ap)
+int script_timer_handlers(int type, void *d)
 {
-        char **__session = va_arg(ap, char**);
-        char **__uid = va_arg(ap, char**);
-        char ***__rcpts = va_arg(ap, char***);
-        char **__text = va_arg(ap, char**);
-        uint32_t **__format = va_arg(ap, uint32_t**);
-        time_t *__sent = va_arg(ap, time_t*);
-        int *__class = va_arg(ap, int*);
-	
-	SCRIPT_HANDLER(message, scr->mask & HAVE_HANDLE_MSG || scr->mask & HAVE_HANDLE_MSG_OWN, __session, __uid, __rcpts, __text, __format, __sent, __class);
+	script_timer_t	*temp	= d;
+	SCRIPT_HANDLER_HEADER(script_handler_timer_t);
+
+	if (!temp->removed && type) { // support na cos czego ekg2 nie ma. i beda memleaki !
+		script_timer_unbind(temp, 0);
+		return 0;
+	} else if (temp->removed > 1) {
+		return -1;
+	}
+
+	SCRIPT_HANDLER_FOOTER(script_handler_timer, type) {
+		temp->removed = 2;
+		if (!type) {
+			timer_remove(NULL, temp->self->name);
+			script_timer_unbind(temp, 0);
+		}
+	}
+
+	return 0;
 }
 
 
-int script_protocol_status(void *data, va_list ap)
+int script_query_handlers(void *data, va_list ap)
 {
-	char **__session = va_arg(ap, char**);
-	char **__uid = va_arg(ap, char**);
-	char **__status = va_arg(ap, char**);
-	char **__descr = va_arg(ap, char**);
+	script_query_t	*temp = data;
+	SCRIPT_HANDLER_HEADER(script_handler_query_t);
 	
-	SCRIPT_HANDLER(status, scr->mask & HAVE_HANDLE_STATUS || scr->mask & HAVE_HANDLE_STATUS_OWN,  __session, __uid, __status, __descr);
-}
+	void 		*args[MAX_ARGS];
+	int		i;
+	
+	for (i=0; i < temp->argc; i++) 
+		args[i] = (void *) va_arg(ap, void *);
+	
+	SCRIPT_HANDLER_FOOTER(script_handler_query, &args);
 
-int script_protocol_connected(void *data, va_list ap)
-{
-	char **__session = va_arg(ap, char**);
-	
-	SCRIPT_HANDLER(connect, scr->mask & HAVE_HANDLE_CONNECT, __session);
-}
-
-int script_keypressed(void *data, va_list ap)
-{
-	int *_ch = va_arg(ap, int*);
-	
-	SCRIPT_HANDLER(keypress, scr->mask & HAVE_HANDLE_KEYPRESS, _ch);
+	return ret;
 }
 
 
-int script_protocol_disconnected(void *data, va_list ap)
-{
-	char **__session = va_arg(ap, char**);
-	
-	SCRIPT_HANDLER(disconnect, scr->mask & HAVE_HANDLE_DISCONNECT, __session);
-}
-
-
-/* scripts main func */
-
+/*******************************************************************************MAIN FUNCTIONSSSSSS*/
 
 /* from python.c  python_autorun() 
  *
@@ -433,7 +644,7 @@ int scripts_autoload(const char *path)
 			xfree(tmp);
 			continue;
 		}
-//		debug("[script] autoloading %s from %s\n", d->d_name, path);
+		debug("[script] autoloading %s from %s\n", d->d_name, path);
 		if (!scriptlang_from_ext(xrindex(tmp, '.'))) { 
 			xfree(tmp);
 			continue;
@@ -447,18 +658,36 @@ int scripts_autoload(const char *path)
 }
 
 
+COMMAND(script_cmd_load) { return script_load(NULL, (char *) params[0]); }
+COMMAND(script_cmd_list) { return script_list(NULL); /* param[0] - slang ? */ }
+COMMAND(script_cmd_varlist) { return script_var_list(NULL); /* param[0] - script ? */ }
+
+COMMAND(cmd_script)
+{
+        if (xstrlen(params[0]) < 1)
+                return script_cmd_list(name, params, session, target, quiet);
+        else {
+		if (!xstrcmp(params[0], "--load")) 
+			return script_load(NULL, (char *) params[1]);
+//            		return script_cmd_load(name, params[1], session, target, quiet);
+		if (!xstrcmp(params[0], "--list"))
+            		return script_list(NULL );
+		if (!xstrcmp(params[0], "--varlist"))
+			return script_var_list(NULL  /* params[1] */);
+	}
+	return -1;
+}
+
 int scripts_init()
 {
 	char *pathtmp;
+        command_add(NULL, "script"        , "p ?", cmd_script, 0, "--list --load --varlist"); /* todo  ?!!? */
+	command_add(NULL, "script:load"   , "f"  , script_cmd_load, 0, "");
+	command_add(NULL, "script:list"   , "?"  , script_cmd_list, 0, "");
+	command_add(NULL, "script:varlist", "?"  , script_cmd_varlist, 0, "");
 	
-        query_connect(NULL, "protocol-message",		script_protocol_message,      NULL);
-        query_connect(NULL, "protocol-status",		script_protocol_status,       NULL);
-        query_connect(NULL, "protocol-connected",	script_protocol_connected,    NULL);
-        query_connect(NULL, "protocol-disconnected",	script_protocol_disconnected, NULL);
-#ifdef HAVE_NCURSES
-	query_connect(NULL, "ui-keypress",		script_keypressed,	      NULL); 
-#endif
-	
+	script_variables_read();
+
 	pathtmp = saprintf("%s/autorun", DATADIR);
 	scripts_autoload(pathtmp);
 	xfree(pathtmp);
@@ -467,9 +696,5 @@ int scripts_init()
 	scripts_autoload(pathtmp);
 	xfree(pathtmp);
 	
-	command_add(NULL, "testdddd", "?", script_command_handlers, 0, NULL);
-	
 	return 0;
-
 }
-
--- ekg/scripts.h
+++ ekg/scripts.h
@@ -1,40 +1,74 @@
 #include <ekg/plugins.h>
 #include <ekg/protocol.h>
+#include <ekg/stuff.h>
+#include <ekg/vars.h>
 
 #include <sys/types.h>
 
 #ifndef EKG_SCRIPTS_H
 #define EKG_SCRIPTS_H
 
-#define SCRIPT_COMMAND_DELETE	-666
+#define SCRIPT_HANDLE_UNBIND    -666
 
-/*      INTERNAME NAME          BITS    sub ekg2_$name in perl script */
+// #define SCR_ARG_CHAR    1
+#define SCR_ARG_CHARP   2 
+#define SCR_ARG_CHARPP  3 
+#define SCR_ARG_INT     4
 
-#define HAVE_HANDLE_MSG         0x01    /* handle_msg */
-#define HAVE_HANDLE_MSG_OWN     0x02    /* handle_msg_own */
-#define HAVE_HANDLE_STATUS      0x04    /* handle_status */
-#define HAVE_HANDLE_STATUS_OWN  0x08    /* handle_status_own */
-#define HAVE_HANDLE_CONNECT     0x10    /* handle_connect */
-#define HAVE_HANDLE_DISCONNECT  0x20    /* handle_disconnect */
-#define HAVE_HANDLE_KEYPRESS    0x40    /* handle_keypress */
-#define HAVE_INIT               0x80    /* init */
-#define HAVE_DEINIT             0x100   /* deinit */
-#define HAVE_ALL                HAVE_DEINIT | HAVE_INIT | HAVE_HANDLE_KEYPRESS | HAVE_HANDLE_DISCONNECT | HAVE_HANDLE_CONNECT | HAVE_HANDLE_STATUS_OWN | HAVE_HANDLE_STATUS | HAVE_HANDLE_MSG_OWN | HAVE_HANDLE_MSG
+#define MAX_ARGS 20
 
+// #define DEBUG(args...) debug(args); /* very verbose; szczegolnie keypress... */
+#define DEBUG(args...) ;
+
+#define SCRIPT_UNBIND_HEADER(x)\
+
+#define SCRIPT_BIND_HEADER(x) \
+        x *temp = xmalloc(sizeof(x)); \
+	\
+	debug("[script_bind] (struct %s) mem = 0x%x scr = 0x%x priv = 0x%x\n", #x, temp, scr, handler);\
+        temp->scr  = scr;\
+	temp->private = handler;
+
+#define SCRIPT_BIND_FOOTER(y) \
+	list_add(&y, temp, 0);\
+	return temp;
+
+#define SCRIPT_BIND_ERROR() \
+	xfree(temp);\
+	return NULL;
+
+#define SCRIPT_HANDLER_HEADER(x) \
+	script_t        *_scr;\
+	scriptlang_t    *_slang;\
+	x		*_handler;\
+	int 		ret = SCRIPT_HANDLE_UNBIND;
+
+#define SCRIPT_HANDLER_FOOTER(y, _args...) \
+	if ((_scr = temp->scr) && ((_slang = _scr->lang)))  _handler = _slang->y;\
+        else                                                _handler = temp->private;\
+        if (_handler)\
+                ret = _handler(_scr, temp, _args); \
+        else {\
+                debug("[%s] (_handler == NULL)\n", #y);\
+                ret = SCRIPT_HANDLE_UNBIND;\
+        }\
+	\
+	if (ret == SCRIPT_HANDLE_UNBIND) { debug("[%s] script or scriptlang want to delete this handler\n", #y); }\
+	if (ret == SCRIPT_HANDLE_UNBIND)
 
 list_t scriptlang;
 list_t scripts;
-list_t script_commands_bindings; /* too long */
 
+list_t script_commands_bindings; /* too long */
+list_t script_timers;
+list_t script_vars;
+list_t script_queries;
 
 typedef struct {
 	void *lang;
 	char *name;
 	char *path;
 	void *private;
-	
-	int mask;
-	
 } script_t;
 
 typedef struct {
@@ -43,26 +77,47 @@ typedef struct {
 	void *private; /* w perlu jest to char *, w pythonie ( !PyObject-= ; char *) .... */
 } script_command_t;
 
+typedef struct {
+	script_t 	*scr;
+	struct timer 	*self;
+	char 		*name;
+	int 		removed;
+	void 		*private;
+} script_timer_t;
+
+typedef struct {
+	script_t 	*scr;
+	variable_t 	*var;
+	char 		*name;
+	char 		*value;
+	void 		*private;
+} script_var_t;
+ 
+typedef struct {
+	script_t 	*scr;
+	char 		*query_name;
+	int 		argc;
+	void 		*private;
+	
+	int             argv_type[MAX_ARGS];
+} script_query_t;
 
 typedef int (scriptlang_initialize_t)();
 typedef int (scriptlang_finalize_t)();
 
-typedef int (script_handler_connected_t)(script_t *, char **);
-typedef int (script_handler_disconnected_t)(script_t *, char **);
-typedef int (script_handler_message_t)(script_t *, char **, char **, char ***, char **, uint32_t **, time_t *, int  *);
-typedef int (script_handler_status_t)(script_t *, char **, char **, char **, char **);
-typedef int (script_handler_keypressed_t)(script_t *, int *);
-
 typedef int (script_load_t)(script_t *);
 typedef int (script_unload_t)(script_t *);
+
 typedef int (script_handler_command_t)(script_t *, script_command_t *, char **);
+typedef int (script_handler_timer_t)  (script_t *, script_timer_t *, int);
+typedef int (script_handler_var_t)    (script_t *, script_var_t *,   char *);
+typedef int (script_handler_query_t)  (script_t *, script_query_t *, void **);
 
 typedef struct {
 	char *name;     // perl, python, php *g* and so on.
 	char *ext; 	//  .pl,    .py, .php ...
 	int  prio;
-	
-	plugin_t *plug;
+	plugin_t *plugin;
 	
 	script_load_t *script_load;
 	script_unload_t *script_unload;
@@ -70,19 +125,13 @@ typedef struct {
 	scriptlang_initialize_t *init;
 	scriptlang_finalize_t   *deinit;
 	
-	script_handler_status_t		*script_handler_status;
-	script_handler_message_t	*script_handler_message;
-	script_handler_disconnected_t	*script_handler_disconnect;
-	script_handler_connected_t	*script_handler_connect;
-	script_handler_keypressed_t     *script_handler_keypress;
+	script_handler_query_t		*script_handler_query;
 	script_handler_command_t        *script_handler_command;
+	script_handler_timer_t 		*script_handler_timer;
+	script_handler_var_t		*script_handler_var;
 	
 	void *private;
 } scriptlang_t;
-
-// #define DEBUG(args...) debug(args); /* very verbose; szczegolnie keypress... */
-#define DEBUG(args...) ;
-
 
 #define SCRIPT_FINDER(bool)\
 	script_t *scr = NULL;\
@@ -96,36 +145,19 @@ typedef struct {
 			return scr;\
 	}\
 	return NULL;
-		
-
-#define SCRIPT_HANDLER(what, bool, args...)\
-	list_t l;\
-	script_t *scr;\
-	scriptlang_t *s;\
-	DEBUG("[script] handling %s\n", #what);\
-	for (l = scripts; l; l = l->next) { \
-		scr = l->data;\
-		s = scr->lang;\
-		if (bool) {\
-			DEBUG("running %s script file %s\n", s->name, scr->path);\
-			s->script_handler_##what(scr, args);\
-		}\
-	}\
-	return 0;
-		
 
 #define SCRIPT_DEFINE(x, y)\
 	extern int x##_load(script_t *);\
 	extern int x##_unload(script_t *);\
-	extern int x##_protocol_disconnected(script_t *, char **);\
-	extern int x##_protocol_connected(script_t *, char **);\
-	extern int x##_protocol_status(script_t *, char **, char **, char **, char **);\
-	extern int x##_protocol_message(script_t *, char **, char **, char ***, char **, uint32_t **, time_t *, int  *);\
-	extern int x##_keypressed(script_t *, int *);\
+	\
 	extern int x##_commands(script_t *, script_command_t *, char **);\
+	extern int x##_timers(script_t *, script_timer_t *, int );\
+	extern int x##_variable_changed(script_t *, script_var_t *, char *);\
+	extern int x##_query(script_t *, script_query_t *, void **);\
+	\
         scriptlang_t x##_lang = { \
                 name: #x, \
-		plug: &x##_plugin, \
+		plugin: &x##_plugin, \
                 ext: y, \
 \
 		init: x##_initialize,\
@@ -134,13 +166,10 @@ typedef struct {
                 script_load: x##_load, \
 		script_unload: x##_unload, \
 \
-		script_handler_keypress: x##_keypressed,\
-		script_handler_connect: x##_protocol_connected,\
-		script_handler_disconnect: x##_protocol_disconnected,\
-		script_handler_status: x##_protocol_status,\
-		script_handler_message: x##_protocol_message,\
-		\
+		script_handler_query  : x##_query,\
 		script_handler_command: x##_commands,\
+		script_handler_timer  : x##_timers,\
+		script_handler_var    : x##_variable_changed,\
         }
 
 #define script_private_get(s) (s->private)
@@ -157,6 +186,17 @@ int scriptlang_unregister(scriptlang_t *s);
 
 int scripts_init();
 
-int script_command_bind(scriptlang_t *s, script_t *scr, char *command, void *handler);
+script_t *script_find(scriptlang_t *s, char *name);
+
+int script_command_unbind(script_command_t *scr_comm, int free);
+int script_timer_unbind(script_timer_t *stimer, int free);
+
+script_command_t *script_command_bind(scriptlang_t *s, script_t *scr, char *command, void *handler);
+script_timer_t *script_timer_bind(scriptlang_t *s, script_t *scr, int freq, void *handler);
+script_query_t *script_query_bind(scriptlang_t *s, script_t *scr, char *query_name, void *handler);
+script_var_t *script_var_add(scriptlang_t *s, script_t *scr, char *name, char *value, void *handler);
+
+int script_variables_free(int free);
+int script_variables_write();
 
 #endif
