eb5331a16ecfdf38be16eeef828b7eb9ef1e6105
Piotr Domagalski <szalik@szalik.net>
2005-09-01 13:18:26 +0000

    oops, one step too far.

--- autoclean.sh
+++ autoclean.sh
@@ -6,7 +6,7 @@ make clean
 
 rm -rf aclocal.m4 ekg2-config.h ekg2-config.h.in config.log config.status configure configure.lineno 
 rm -rf install-sh autom4te.cache config.guess config.sub libtool ltmain.sh stamp-h1 depcomp missing mkinstalldirs
-rm -rf compile config.rpath libltdl po
+rm -rf compile config.rpath libltdl
 
 find . \( -name Makefile -o -name Makefile.in -o -name \*.la -o -name \*.a -o -name \*.so -o -name \*.o -type f \) -exec rm -f {} \;
 find . \( -name \.deps -o -name \.libs -type d \) -exec rm -rf {} \;
