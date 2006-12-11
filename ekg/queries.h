a71f87b577c0d28d5e3f1e92dd8d6667e4e7eb16
Jakub Zawadzki <darkjames@darkjames.ath.cx>
2006-12-11 23:00:44 +0000

    some more query types added to queries_list[]

--- ekg/queries.h
+++ ekg/queries.h
@@ -10,8 +10,6 @@ enum query_arg_type {
 	QUERY_ARG_CHARPP,	/* char ** 	*/
 	QUERY_ARG_INT, 		/* int */
 	QUERY_ARG_UINT,		/* unsgined int */		/* -> time_t, uint32_t */
-			/* sizeof(unsigned int) == sizeof(int) nie wiadomo po co, moze wyleci... */
-	
 	QUERY_ARG_WINDOW = 100, /* window_t	*/
 	QUERY_ARG_FSTRING, 	/* fstring_t	*/
 };
@@ -24,17 +22,46 @@ struct query {
 
 /* uniq id of known queries... keep it sorted.. please */
 enum queries_id {
-	PROTOCOL_CONNECTED = 0,
+	IRC_TOPIC = 0,
+	MAIL_COUNT,
+
+	CONFIG_POSTINIT,
+
+	PROTOCOL_CONNECTED,
 	PROTOCOL_DISCONNECTED,
 	PROTOCOL_MESSAGE,
 	PROTOCOL_STATUS,
 
-	QUERY_INTERNAL,
+	UI_BEEP,
+	UI_IS_INITIALIZED,
+	UI_KEYPRESS,
+	UI_LOOP,
+	UI_WINDOW_ACT_CHANGED,
+	UI_WINDOW_KILL,
+	UI_WINDOW_NEW,
+	UI_WINDOW_PRINT,
+
+	QUERY_EXTERNAL,
 };
 
 /* list of known queries. keep it sorted too. */
+#ifdef __DECLARE_QUERIES_STUFF
+#undef __DECLARE_QUERIES_STUFF
 
-struct query queries[] = {
+struct query query_list[] = {
+	{ IRC_TOPIC, "irc-topic", {
+		QUERY_ARG_CHARP,		/* if CHANNEL -> topic; 	if USER -> ident@host */
+		QUERY_ARG_CHARP,		/* if CHANNEL -> topicby;	if USER -> realname */
+		QUERY_ARG_CHARP,		/* if CHANNEL -> chanmodes;	if USER -> undefined */
+		QUERY_ARG_END } },
+
+	{ MAIL_COUNT, "mail-count", {
+		QUERY_ARG_INT,			/* mail count */
+		QUERY_ARG_END } },
+
+	{ CONFIG_POSTINIT, "config-postinit", {
+		QUERY_ARG_END } },		/* no params */
+
 	{ PROTOCOL_CONNECTED, "protocol-connected", {
 		QUERY_ARG_CHARP,		/* session */
 		QUERY_ARG_END } }, 
@@ -68,16 +95,49 @@ struct query queries[] = {
 		QUERY_ARG_UINT, /* time_t */	/* when */
 		QUERY_ARG_END } }, 
 
-	{ QUERY_UNKNOWN, (void *) 0, { }  }
+	{ UI_BEEP, "ui-beep", {
+		QUERY_ARG_END } }, 		/* no params */
+
+	{ UI_IS_INITIALIZED, "ui-is-initialized", {
+		QUERY_ARG_INT,			/* is_ui */
+		QUERY_ARG_END } }, 
+
+	{ UI_KEYPRESS, "ui-keypress", {
+		QUERY_ARG_UINT,	 		/* key */
+		QUERY_ARG_END } },
+
+	{ UI_LOOP, "ui-loop", {
+		QUERY_ARG_END } }, 		/* no params */
+
+	{ UI_WINDOW_ACT_CHANGED, "ui-window-act-changed", {
+		QUERY_ARG_END } },		/* no params */
+
+	{ UI_WINDOW_KILL, "ui-window-kill", {
+		QUERY_ARG_WINDOW,		/* window */
+		QUERY_ARG_END } },
+
+	{ UI_WINDOW_NEW, "ui-window-new", {
+		QUERY_ARG_WINDOW,		/* window */
+		QUERY_ARG_END } }, 
+
+	{ UI_WINDOW_PRINT, "ui-window-print", {
+		QUERY_ARG_WINDOW,		/* window */
+		QUERY_ARG_FSTRING,		/* fstring_t */
+		QUERY_ARG_END } }, 
 };
 
-/* other, not listed here queries, for example plugin which use internally his own query, and if devel of that plugin doesn't want share with us info about that plugin..
- * 	can use query_connect() query_emit() and it will work... however, binding that query from scripts won't work.. untill devel fill query_arg_type...
+/* other, not listed above here queries, for example plugin which use internally his own query, 
+ * and if devel of that plugin doesn't want share with us info about that plugin..
+ * can use query_connect() query_emit() and it will work... however, binding that query from scripts won't work.. untill devel fill query_arg_type...
  */
 
-extern list_t queries_other;
-extern int next_internal_query = QUERY_INTERNAL;
+static list_t queries_external;
+static int queries_count = QUERY_EXTERNAL;	/* list_count(queries_other)+QUERY_EXTERNAL */
+#else
 
+extern struct query query_list[];	/* for scripts.h */
+
+#endif
 
 #endif
 
