34e8489809754cc2209bed2e10d0dce0ff5b1cf9
Jakub Zawadzki <darkjames@darkjames.ath.cx>
2006-12-12 08:29:38 +0000

    a lot of new stuff.

--- ekg/queries.h
+++ ekg/queries.h
@@ -1,7 +1,7 @@
 #ifndef __EKG_QUERIES
 #define __EKG_QUERIES
 
-#define QUERY_ARGS_MAX 15
+#define QUERY_ARGS_MAX 12
 
 enum query_arg_type {
 	QUERY_ARG_END = 0,	/* MUSTBE LAST ELEMENT OF `query_arg_type` */
@@ -26,27 +26,34 @@ enum queries_id {
 	MAIL_COUNT,
 
 	CONFIG_POSTINIT,
+	DAY_CHANGED,
 
-	PROTOCOL_CONNECTED,
-	PROTOCOL_DISCONNECTED,
-	PROTOCOL_MESSAGE,
-	PROTOCOL_STATUS,
+	METACONTACT_ADDED, METACONTACT_REMOVED,
 
-	UI_BEEP,
-	UI_IS_INITIALIZED,
-	UI_KEYPRESS,
-	UI_LOOP,
-	UI_WINDOW_ACT_CHANGED,
-	UI_WINDOW_KILL,
-	UI_WINDOW_NEW,
-	UI_WINDOW_PRINT,
+	PLUGIN_PRINT_VERSION,
+
+	PROTOCOL_CONNECTED, PROTOCOL_DISCONNECTED, PROTOCOL_IGNORE, PROTOCOL_MESSAGE, PROTOCOL_MESSAGE_ACK, PROTOCOL_STATUS, PROTOCOL_VALIDATE_UID, PROTOCOL_XSTATE,
+
+	SESSION_ADDED, SESSION_REMOVED, SESSION_CHANGED, SESSION_RENAMED,
+
+	SET_VARS_DEFAULT,
+	STATUS_SHOW,
+
+	UI_BEEP, UI_IS_INITIALIZED, UI_KEYPRESS, UI_LOOP, UI_WINDOW_ACT_CHANGED,
+	UI_WINDOW_CLEAR, UI_WINDOW_KILL, UI_WINDOW_NEW, UI_WINDOW_PRINT, UI_WINDOW_REFRESH,
+	UI_WINDOW_SWITCH, UI_WINDOW_TARGET_CHANGED,
+
+	USERLIST_ADDED, USERLIST_CHANGED, USERLIST_REMOVED,
+
+	VARIABLE_CHANGED,
 
 	QUERY_EXTERNAL,
 };
 
-/* list of known queries. keep it sorted too. */
 #ifdef __DECLARE_QUERIES_STUFF
 #undef __DECLARE_QUERIES_STUFF
+
+/* list of known queries. keep it sorted too. */
 
 struct query query_list[] = {
 	{ IRC_TOPIC, "irc-topic", {
@@ -62,6 +69,20 @@ struct query query_list[] = {
 	{ CONFIG_POSTINIT, "config-postinit", {
 		QUERY_ARG_END } },		/* no params */
 
+	{ DAY_CHANGED, "day-changed", {		/* XXX: struct tm *, struct tm * 	*/
+		QUERY_ARG_END } },
+
+	{ METACONTACT_ADDED, "metacontact-added", {
+		QUERY_ARG_CHARP,		/* metacontact name */
+		QUERY_ARG_END } },
+
+	{ METACONTACT_REMOVED, "metacontact-removed", {
+		QUERY_ARG_CHARP,		/* metacontact name */
+		QUERY_ARG_END } },
+
+	{ PLUGIN_PRINT_VERSION, "plugin-print-version", {
+		QUERY_ARG_END } },		/* no params */
+
 	{ PROTOCOL_CONNECTED, "protocol-connected", {
 		QUERY_ARG_CHARP,		/* session */
 		QUERY_ARG_END } }, 
@@ -72,8 +93,15 @@ struct query query_list[] = {
 		QUERY_ARG_INT,			/* type */
 		QUERY_ARG_END } }, 
 
+	{ PROTOCOL_IGNORE, "protocol-ignore", {
+		QUERY_ARG_CHARP,		/* session uid */
+		QUERY_ARG_CHARP,		/* uid */
+		QUERY_ARG_INT,			/* oldlevel */
+		QUERY_ARG_INT,			/* newlevel */
+		QUERY_ARG_END } },
+
 	{ PROTOCOL_MESSAGE, "protocol-message", {
-		QUERY_ARG_CHARP,		/* session */
+		QUERY_ARG_CHARP,		/* session uid */
 		QUERY_ARG_CHARP,		/* uid */
 		QUERY_ARG_CHARPP,		/* rcpts */
 		QUERY_ARG_CHARP,		/* text */
@@ -85,8 +113,15 @@ struct query query_list[] = {
 		QUERY_ARG_INT,			/* secure */
 		QUERY_ARG_END } },
 
+	{ PROTOCOL_MESSAGE_ACK, "protocol-message-ack", {
+		QUERY_ARG_CHARP,		/* session uid */
+		QUERY_ARG_CHARP,		/* uid */
+		QUERY_ARG_CHARP,		/* seq */
+		QUERY_ARG_CHARP,		/* status */
+		QUERY_ARG_END } },
+
 	{ PROTOCOL_STATUS, "protocol-status", {
-		QUERY_ARG_CHARP,		/* session */
+		QUERY_ARG_CHARP,		/* session uid */
 		QUERY_ARG_CHARP,		/* uid */
 		QUERY_ARG_CHARP,		/* status */
 		QUERY_ARG_CHARP,		/* descr */
@@ -94,6 +129,40 @@ struct query query_list[] = {
 		QUERY_ARG_INT,			/* port */
 		QUERY_ARG_UINT, /* time_t */	/* when */
 		QUERY_ARG_END } }, 
+
+	{ PROTOCOL_VALIDATE_UID, "protocol-validate-uid", {
+		QUERY_ARG_CHARP,		/* uid */
+		QUERY_ARG_INT,			/* valid */
+		QUERY_ARG_END } },
+
+	{ PROTOCOL_XSTATE, "protocol-xstate", {
+		QUERY_ARG_CHARP,		/* session */
+		QUERY_ARG_CHARP,		/* uid */
+		QUERY_ARG_INT,			/* state 	- bits on */
+		QUERY_ARG_INT,			/* offstate 	- bits off */
+		QUERY_ARG_END } },
+	
+	{ SESSION_ADDED, "session-added", {
+		QUERY_ARG_CHARP,		/* session uid */
+		QUERY_ARG_END } },
+
+	{ SESSION_CHANGED, "session-changed", {
+		QUERY_ARG_END } },		/* no params */
+
+	{ SESSION_REMOVED, "session-removed", {
+		QUERY_ARG_CHARP,		/* session uid */
+		QUERY_ARG_END } },
+
+	{ SESSION_RENAMED, "session-renamed", {
+		QUERY_ARG_CHARP,		/* new session alias */
+		QUERY_ARG_END } },
+
+	{ SET_VARS_DEFAULT, "set-vars-default", {
+		QUERY_ARG_END } },		/* no params */
+
+	{ STATUS_SHOW, "status-show", {
+		QUERY_ARG_CHARP,		/* session uid */
+		QUERY_ARG_END } },
 
 	{ UI_BEEP, "ui-beep", {
 		QUERY_ARG_END } }, 		/* no params */
@@ -112,6 +181,10 @@ struct query query_list[] = {
 	{ UI_WINDOW_ACT_CHANGED, "ui-window-act-changed", {
 		QUERY_ARG_END } },		/* no params */
 
+	{ UI_WINDOW_CLEAR, "ui-window-clear", {
+		QUERY_ARG_WINDOW,		/* window */
+		QUERY_ARG_END } },
+
 	{ UI_WINDOW_KILL, "ui-window-kill", {
 		QUERY_ARG_WINDOW,		/* window */
 		QUERY_ARG_END } },
@@ -124,6 +197,41 @@ struct query query_list[] = {
 		QUERY_ARG_WINDOW,		/* window */
 		QUERY_ARG_FSTRING,		/* fstring_t */
 		QUERY_ARG_END } }, 
+
+	{ UI_WINDOW_REFRESH, "ui-window-refresh", {
+		QUERY_ARG_END } },		/* no params */
+
+	{ UI_WINDOW_SWITCH, "ui-window-switch", {
+		QUERY_ARG_WINDOW,		/* window */
+		QUERY_ARG_END } },
+
+	{ UI_WINDOW_TARGET_CHANGED , "ui-window-target-changed", {
+		QUERY_ARG_WINDOW,		/* window */
+		QUERY_ARG_END } },
+
+	{ USERLIST_ADDED, "userlist-added", {
+		/* XXX, we need here a session->uid too (?) */
+
+		QUERY_ARG_CHARP,		/* uid */
+		QUERY_ARG_CHARP,		/* nickname */
+		QUERY_ARG_INT,			/* quiet */
+		QUERY_ARG_END } },
+
+	{ USERLIST_CHANGED, "userlist-changed", {
+		QUERY_ARG_CHARP,		/* session uid */
+		QUERY_ARG_CHARP,		/* uid */
+		QUERY_ARG_END } },
+
+	{ USERLIST_REMOVED, "userlist-removed", {
+		/* XXX, we need here a session->uid too (?) */
+
+		QUERY_ARG_CHARP,		/* nickname or uid */
+		QUERY_ARG_CHARP,		/* uid */
+		QUERY_ARG_END } },
+
+	{ VARIABLE_CHANGED, "variable-changed", {
+		QUERY_ARG_CHARP,		/* variable */
+		QUERY_ARG_END } },
 };
 
 /* other, not listed above here queries, for example plugin which use internally his own query, 
