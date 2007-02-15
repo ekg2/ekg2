444c7e02361c6f2eace3a8ce8900457fa61626ed
Jakub Zawadzki <darkjames@darkjames.ath.cx>
2007-02-15 11:07:34 +0000

    add copyright notes for: sniff_gg.h (libgadu + protocol documentation) if someone [don't want]/[want] be put in copyright notes. please give me sign.

--- plugins/sniff/sniff_gg.h
+++ plugins/sniff/sniff_gg.h
@@ -1,3 +1,33 @@
+/*
+ *  (C) Copyright 2001-2003 Wojtek Kaniewski <wojtekka@irc.pl>
+ *                          Robert J. Woźny <speedy@ziew.org>
+ *                          Arkadiusz Miśkiewicz <arekm@pld-linux.org>
+ *                          Tomasz Chiliński <chilek@chilan.com>
+ *                          Piotr Wysocki <wysek@linux.bydg.org>
+ *                          Dawid Jarosz <dawjar@poczta.onet.pl>
+ *
+ *			& many others look at: http://ekg.chmurka.net/docs/protocol.html
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License Version
+ *  2.1 as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ */
+
+/* struct data && magic constants copied from libgadu (http://toxygen.net/libgadu) && 
+ * 	gg protocol documentation (http://ekg.chmurka.net/docs/protocol.html)
+ * 	great job guys! thx.
+ */
+
 #include <sys/types.h>
 
 #define GG_PACKED __attribute__ ((packed))
