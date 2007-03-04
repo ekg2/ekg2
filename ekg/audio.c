508f646aa4765fd02bfdb47277d2171d89016781
Jakub Zawadzki <darkjames@darkjames.ath.cx>
2007-03-04 12:34:39 +0000

    move WAVE structs to other file.. _all_ code associated with wave will be moved too... after it, we'll do audio_ogg.h ;)

--- ekg/audio.c
+++ ekg/audio.c
@@ -35,57 +35,13 @@
 #include "stuff.h"
 #include "xmalloc.h"
 
+#include "audio_wav.h"
+
 list_t audio_codecs;
 list_t audio_inputs;
 list_t streams;
 
 AUDIO_DEFINE(stream);
-
-/* *.wav I/O stolen from xawtv (recode program) which was stolen from cdda2wav */
-/* Copyright (C) by Heiko Eissfeldt */
-
-typedef uint8_t   BYTE;
-typedef uint16_t  WORD;
-typedef uint32_t  DWORD;
-typedef uint32_t  FOURCC;	/* a four character code */
-
-/* flags for 'wFormatTag' field of WAVEFORMAT */
-#define WAVE_FORMAT_PCM 1
-
-/* MMIO macros */
-#define mmioFOURCC(ch0, ch1, ch2, ch3) \
-  ((DWORD)(BYTE)(ch0) | ((DWORD)(BYTE)(ch1) << 8) | \
-  ((DWORD)(BYTE)(ch2) << 16) | ((DWORD)(BYTE)(ch3) << 24))
-
-#define FOURCC_RIFF	mmioFOURCC ('R', 'I', 'F', 'F')
-#define FOURCC_LIST	mmioFOURCC ('L', 'I', 'S', 'T')
-#define FOURCC_WAVE	mmioFOURCC ('W', 'A', 'V', 'E')
-#define FOURCC_FMT	mmioFOURCC ('f', 'm', 't', ' ')
-#define FOURCC_DATA	mmioFOURCC ('d', 'a', 't', 'a')
-
-typedef struct CHUNKHDR {
-    FOURCC ckid;		/* chunk ID */
-    DWORD dwSize; 	        /* chunk size */
-} CHUNKHDR;
-
-/* simplified Header for standard WAV files */
-typedef struct WAVEHDR {
-    CHUNKHDR chkRiff;
-    FOURCC fccWave;
-    CHUNKHDR chkFmt;
-    WORD wFormatTag;	   /* format type */
-    WORD nChannels;	   /* number of channels (i.e. mono, stereo, etc.) */
-    DWORD nSamplesPerSec;  /* sample rate */
-    DWORD nAvgBytesPerSec; /* for buffer estimation */
-    WORD nBlockAlign;	   /* block size of data */
-    WORD wBitsPerSample;
-    CHUNKHDR chkData;
-} WAVEHDR;
-
-#define cpu_to_le32(x) (x)
-#define cpu_to_le16(x) (x)
-#define le32_to_cpu(x) (x)
-#define le16_to_cpu(x) (x)
 
 /*********************************************************************************/
 
