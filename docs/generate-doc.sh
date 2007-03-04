0374f053844b040eaa88d4ac8066515d44cb6c8d
Jakub Zawadzki <darkjames@darkjames.ath.cx>
2007-03-04 13:14:08 +0000

    new version

--- docs/generate-doc.sh
+++ docs/generate-doc.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# XXX, INPUT_ENCODING nie wiedziec czemu nie dziala.
+# 	dlatego tymczasowo OUTPUT_LANGUAGE jest Polish
+#	co powoduje zmiane kodowania strony na ISO-8859-2
+#
+#
+
 rm -rf ./doxygen
 
 echo -ne \
@@ -9,9 +15,12 @@ echo -ne \
 	\
 	"INPUT=../\n" \
 	"INPUT_ENCODING=ISO-8859-2\n" \
-	"EXTRACT_STATIC=YES\n" \
+	"OUTPUT_LANGUAGE=Polish\n" \
 	"RECURSIVE=YES\n" \
 	"EXTRACT_ALL=YES\n" \
+	"EXTRACT_STATIC=YES\n" \
+	"FULL_PATH_NAMES=YES\n" \
+	"STRIP_FROM_PATH=`dirname ${PWD}`\n" \
 	\
 	"QUIET=YES\n" \
 	"WARNINGS=YES\n" \
