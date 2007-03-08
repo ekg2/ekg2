3255a6c683ccd41d0837d80f50cfdb8431d523f7
Jakub Zawadzki <darkjames@darkjames.ath.cx>
2007-03-08 14:30:28 +0000

    doc irc_onkick_handler() change it a bit.

--- plugins/irc/autoacts.c
+++ plugins/irc/autoacts.c
@@ -28,6 +28,29 @@
 extern plugin_t irc_plugin; 
 static TIMER(irc_autorejoin_timer);
 
+/**
+ * irc_onkick_handler()
+ *
+ * Handler for: <i>IRC_KICK</i><br>
+ *
+ * Here we check if we were kicked (by checking @a nick) and if yes, than if we want to rejoin on kick (REJOIN && REJOIN_TIME)<br>
+ * Than after time specified by REJOIN_TIME we try to rejoin
+ *
+ * @sa irc_autorejoin()		- for rejoin function
+ * @sa irc_autorejoin_timer()	- for rejoin timer.
+ *
+ * @todo	We don't check if @a nick and @a chan is full uid.. It's I think correct.. However can be faulty.
+ *
+ * @param	ap 1st param: <i>(char *) </i><b>session</b> 	- session uid
+ * 		ap 2nd param: <i>(char *) </i><b>nick</b>	- full uid of kicked person (irc:nickname)
+ * 		ap 3rd param: <i>(char *) </i><b>chan</b>	- full uid of channel where kick event happen.
+ * 		ap 4th param: <i>(char *) </i><b>kickedby</b>	- full uid who kicked.
+ *
+ * @return 	1 - If no session, no irc session, or no private struct.
+ * 		2 - If we are not interested in autorejoining	[either smb else was kicked (not us) or REJOIN was not set]
+ * 		3 - If we'll try to autorejoin
+ */
+
 QUERY(irc_onkick_handler) {
 	char *session	= *va_arg(ap, char **);
 	char *nick	= *va_arg(ap, char **);
@@ -35,27 +58,43 @@ QUERY(irc_onkick_handler) {
 	char *kickedby	= *va_arg(ap, char **);
 
 	session_t     *s = session_find(session);
-	irc_private_t *j = irc_private(s);
-	
-	if (!xstrcmp(j->nick, nick+4))
-	{
+	irc_private_t *j;
+
+	if (!s || !(j = s->priv) || s->plugin != &irc_plugin)
+		return 1;
+
+	if (!xstrcmp(j->nick, nick+4)) {
 		int rejoin = session_int_get(s, "REJOIN");
+
+		if (rejoin < 0)
+			return 2;
+
 		if (rejoin&(1<<(IRC_REJOIN_KICK))) {
+			int rejoin_time = session_int_get(s, "REJOIN_TIME");
+
+			/* if it's negative value, fix it. cause we set REJOIN... */
+			if (rejoin_time < 0) 
+				rejoin_time = 0;
+
+			/* if we don't want to wait after kick, we do it here. not in next ekg_loop() */
+			if (rejoin_time == 0) {
+				irc_autorejoin(s, IRC_REJOIN_KICK, chan+4);
+				return 3;
+			}
+
 			irc_onkick_handler_t *data = xmalloc(sizeof(irc_onkick_handler_t));
 
 			data->s		= s;
 			data->nick 	= xstrdup(nick);
 			data->kickedby 	= xstrdup(kickedby);
 			data->chan 	= xstrdup(chan);
-			
-			timer_add(&irc_plugin, NULL, 
-					session_int_get(s, "REJOIN_TIME"),
-					0, irc_autorejoin_timer, data);
-			return 0;
+
+			timer_add(&irc_plugin, NULL, rejoin_time, 0, irc_autorejoin_timer, data);
+			return 3;
 		}
 	} 
 	
-	return 0;
+	return 2;
 }
 
 static TIMER(irc_autorejoin_timer)
