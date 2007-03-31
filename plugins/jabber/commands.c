64f4bf462ff0e79118d0e3df0128f1bf865f3728
Michał Górny <mgorny@gentoo.org>
2007-03-31 14:19:22 +0000

    Updating use of config_display_ack.

--- plugins/jabber/commands.c
+++ plugins/jabber/commands.c
@@ -611,9 +611,8 @@ static COMMAND(jabber_command_msg)
 
 	if (!j->istlen) 
 		watch_write(j->send_watch, "<x xmlns=\"jabber:x:event\">%s%s<displayed/><composing/></x>", 
-			( config_display_ack == 1 || config_display_ack == 2 ? "<delivered/>" : ""),
-			( config_display_ack == 1 || config_display_ack == 3 ? "<offline/>"   : "") );
-	else ;
+			( config_display_ack & 1 ? "<delivered/>" : ""),
+			( config_display_ack & 2 ? "<offline/>"   : "") );
 
 	watch_write(j->send_watch, "</message>");
 	JABBER_COMMIT_DATA(j->send_watch);
