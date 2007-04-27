b31632b809ad104d6aedf6e9a1d2cc632e52b61f
Michał Górny <mgorny@gentoo.org>
2007-04-27 13:29:35 +0000

    Trying to update/fix.

--- plugins/perl/perl_bless.c
+++ plugins/perl/perl_bless.c
@@ -150,8 +150,10 @@ void ekg2_bless_user(HV *hv, userlist_t *user)
 	debug_bless("blessing user %s\n", user->uid);
 	hv_store(hv, "uid", 3, new_pv(user->uid), 0);
 	hv_store(hv, "nickname", 8, new_pv(user->nickname), 0);
-	hv_store(hv, "status", 6, new_pv(user->status), 0);
+	hv_store(hv, "status", 6, new_pv(ekg_status_string(user->status, 0)), 0);
+#if 0 /* XXX? */
 	hv_store(hv, "ip", 2, 	  new_pv(inet_ntoa_u(user->ip)), 0);
+#endif
 }
 
 void ekg2_bless_session(HV *hv, session_t *session)
@@ -159,7 +161,7 @@ void ekg2_bless_session(HV *hv, session_t *session)
         debug_bless("blessing session %s\n", session->uid);
         hv_store(hv, "connected", 9, newSViv(session->connected), 0);
         hv_store(hv, "uid",       3, new_pv(session->uid), 0);
-	hv_store(hv, "status",	  6, new_pv(session->status), 0);
+	hv_store(hv, "status",	  6, new_pv(ekg_status_string(session->status, 0)), 0);
 //	hv_store(hv, "userlist",  8, create_sv_ptr(session->userlist), 0);
 //	hv_store(hv, "userlist",  8, ekg2_bless(BLESS_LIST, 1, session->userlist), 0); // obsolete call userlist()
 	hv_store(hv, "alias",	  5, new_pv(session->alias), 0);
