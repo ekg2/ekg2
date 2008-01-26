cfc40fa6019f1f11f0989eb1c734334c045724c6
Jakub Zawadzki <darkjames@darkjames.ath.cx>
2008-01-26 21:01:16 +0000

    [ruby] sample for command and timers.

--- contrib/ruby/Sample.rb
+++ contrib/ruby/Sample.rb
@@ -7,16 +7,27 @@ MSG
 end
 
 class Ekg2::Script::Sample < Ekg2::Script
+	def handler_foo(parametr = nil)
+		if parametr != nil
+			print "Wywolane polecenie foo z parametrem: `" + parametr + "` !"
+		else
+			print "Wywolane polecenie foo bez parametrow!"
+		end
+	end
+
 	def handler_keypress(klawisz)
-		print "Nacisnales klawisz!: #" + klawisz.to_s;
+		print "Nacisnales klawisz!: #" + klawisz.to_s
+	end
+
+	def handler_czasomierz()
+		print "Czas plynie a dekoral wciaz bialy"
 	end
 
 	def initialize
 		super
-		handler_bind("ui-keypress", "handler_keypress")
+#		handler_bind("ui-keypress", "handler_keypress")
+		command_bind("foo", "handler_foo")
+		timer_bind(1, "handler_czasomierz");
 	end
 
-	def foo
-		print "super"
-	end
 end
