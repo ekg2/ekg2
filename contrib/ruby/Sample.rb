f6892853c7c960165384f2a55cb99c5ce48cf4cf
Jakub Zawadzki <darkjames@darkjames.ath.cx>
2008-01-26 20:31:17 +0000

    sample script for ruby.

---
+++ contrib/ruby/Sample.rb
@@ -0,0 +1,22 @@
+if $0 != "ekg2"
+	print <<MSG
+	warning: you are executing an embedded ruby file!
+	this file is suppose to be run only from ekg2.
+MSG
+	exit
+end
+
+class Ekg2::Script::Test < Ekg2::Script
+	def handler_keypress(klawisz)
+		print "Nacisnales klawisz!: #" + klawisz.to_s;
+	end
+
+	def initialize
+		super
+		handler_bind("ui-keypress", "handler_keypress")
+	end
+
+	def foo
+		print "super"
+	end
+end
