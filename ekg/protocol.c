1d2e0aced77523f7a4f93060d35e755d68173274
Wiesław Ochmiński <wiechu@wiechu.com>
2008-05-24 20:37:09 +0000

    Support '/ignore ... notify'.

--- ekg/protocol.c
+++ ekg/protocol.c
@@ -406,7 +406,7 @@ static QUERY(protocol_status)
 		play_sound(config_sound_notify_file);
 
         /* wy¶wietlaæ na ekranie? */
-	if (!((sess_notify == -1 ? config_display_notify : sess_notify) & 3))
+	if (!((sess_notify == -1 ? config_display_notify : sess_notify) & 3) || ignore_notify)
                 goto notify_plugins;
 
 	/* poka¿ */
