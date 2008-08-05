964a88f0905b7a9b2aabb1fd3cbfe99fe28c12eb
Jakub Zawadzki <darkjames@darkjames.ath.cx>
2008-08-05 22:56:05 +0000

    initial support for simple messages.

--- plugins/icq/icq_snac_handlers_04message.c
+++ plugins/icq/icq_snac_handlers_04message.c
@@ -77,14 +77,147 @@ flags = 0x03;
 	return 0;
 }
 
+int icq_snac_message_recv_simple(session_t *s, unsigned char *buf, int len, const char *sender) {
+	struct icq_tlv_list *tlvs;
+	struct icq_tlv_list *tlvs_msg;
+	icq_tlv_t *t;
+	
+	debug_function("icq_snac_message_recv_simple() from: %s leftlen: %d\n", sender, len);
+
+	if (!(tlvs = icq_unpack_tlvs(buf, len, 0))) {
+		debug("icq_snac_message_recv_simple() ignoring empty message..\n");
+		return 0;
+	}
+	
+	if (!(t = icq_tlv_get(tlvs, 0x02)) || t->type != 0x02) {
+		debug_error("icq_snac_message_recv_simple() TLV(0x02) not found?\n");
+		icq_tlvs_destroy(&tlvs);
+		return 1;
+	}
+
+	if (!(tlvs_msg = icq_unpack_tlvs(t->buf, t->len, 0))) {
+		debug_error("icq_snac_message_recv_simple() failed to read tlv chain in message\n");
+		icq_tlvs_destroy(&tlvs);
+		return 1;
+	}
+
+	if ((t = icq_tlv_get(tlvs_msg, 0x501))) {
+		debug("icq_snac_message_recv_simple() message has: %d caps\n", t->len);
+	} else
+		debug("icq_snac_message_recv_simple() no message cap\n");
+
+	{
+		/* Parse the message parts, usually only one 0x0101 TLV containing the message,
+		 * but in some cases there can be more 0x0101 TLVs containing message parts in
+		 * different encoding (just like the new format of Offline Messages) */ 
+		struct icq_tlv_list *t;
+		string_t msg = string_init(NULL);
+		time_t sent;
+
+		for (t = tlvs_msg; t; t = t->next) {
+			struct {
+				uint16_t encoding;
+				uint16_t codepage;
+				unsigned char *message;
+			} t_msg;
+
+			int t_len = t->len;
+
+			if (t->type != 0x0101)
+				continue;
+
+			if (!icq_unpack(t->buf, &t_msg.message, &t_len, "ww", &t_msg.encoding, &t_msg.codepage))
+				continue;
+
+			debug_function("icq_snac_message_recv_simple() enc: %.4x cp: %.4x\n", t_msg.encoding, t_msg.codepage);
+
+			switch (t_msg.encoding) {
+				case 0x02:
+				{
+					/* XXX, UCS-2 */
+					debug_error("icq_snac_message_recv_simple() message in UCS-2 !!! not supported, yet, sorry :(\n");
+					icq_hexdump(DEBUG_ERROR, t_msg.message, t_len);
+					break;
+				}
+				case 0x00:	/* US-ASCII */
+				case 0x03:	/* ANSI */
+				default:
+				{
+					string_append_n(msg, t_msg.message, t_len);
+				}
+
+				/* XXX, recode */
+			}
+		}
+
+		/* XXX, check if message was recv when we was offline */
+
+		sent = time(NULL);
+
+		if (msg->len) {
+			/* XXX, class? */
+			protocol_message_emit(s, sender, NULL, msg->str, NULL, sent, EKG_MSGCLASS_CHAT, NULL, EKG_TRY_BEEP, 0);
+		}
+
+		string_free(msg, 1);
+	}
+
+	icq_tlvs_destroy(&tlvs);
+	icq_tlvs_destroy(&tlvs_msg);
+	return 0;
+}
+
 SNAC_SUBHANDLER(icq_snac_message_recv) {
-	if (len < 11) {
-		debug_error("SNAC_04_07() Malformed message thru server\n");
+	struct {
+		uint32_t id1;
+		uint32_t id2;
+		uint16_t format;	/* 0x01 - simple message format
+					   0x02 - advanced message format
+					   0x04 - 'new' message format
+					 */
+		char *sender;
+		uint16_t warning_level;	/* not used */
+		uint16_t tlv_count;
+	} pkt;
+
+	if (!ICQ_UNPACK(&buf, "iiWuWW", &pkt.id1, &pkt.id2, &pkt.format, &pkt.sender, &pkt.warning_level, &pkt.tlv_count)) {
+		debug_error("icq_snac_message_recv() Malformed message thru server\n");
 		return -1;
 	}
 
-	debug_error("icq_snac_message_recv() XXX\n");
-	return -3;
+	debug_function("icq_snac_message_recv() from: %s id1: %.8x id2: %.8x format: %.4x warning: %.4x tlvs: %.4x\n",
+				pkt.sender, pkt.id1, pkt.id2, pkt.format, pkt.warning_level, pkt.tlv_count);
+
+	/* XXX, spamer? */
+	while (pkt.tlv_count) {
+		uint16_t t_type, t_len;
+
+		/* XXX, przerobic icq_unpack_tlvs() zeby uaktualnialo buf i len!!! */
+
+		if (!ICQ_UNPACK(&buf, "WW", &t_type, &t_len))
+			return -1;
+		if (len < t_len)
+			return -1;
+
+		buf += t_len; len -= t_len;
+
+		pkt.tlv_count--;
+	}
+
+	switch (pkt.format) {
+		case 0x01:
+			icq_snac_message_recv_simple(s, buf, len, pkt.sender);
+			break;
+
+		case 0x02:
+		case 0x04:
+		default:
+			debug_error("icq_snac_message_recv() unknown format message from server sender: %s\n", pkt.sender);
+			/* dump message */
+			icq_hexdump(DEBUG_ERROR, buf, len);
+			break;
+	}
+	return 0;
 }
 
 SNAC_SUBHANDLER(icq_snac_message_queue) {	/* SNAC(4, 0x17) Offline Messages response */
