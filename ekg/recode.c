a77f160c8595b81ae455029b4f17082fbc707136
Jakub Zawadzki <darkjames@darkjames.ath.cx>
2008-09-03 13:54:18 +0000

    cleanup recode file a little.

--- ekg/recode.c
+++ ekg/recode.c
@@ -473,7 +473,7 @@ int ekg_converters_display(int quiet) {
 #endif
 }
 
-static int utf8_helper(unsigned char *s, int n, unsigned short *ch) {
+static int ekg_utf8_helper(unsigned char *s, int n, unsigned short *ch) {
 	unsigned char c = s[0];
 
 	if (c < 0x80) {
@@ -507,26 +507,108 @@ invalid:
 	return 1;
 }
 
+static char *ekg_from_utf8(char *b, const unsigned short *recode_table) {	/* sizeof(recode_table) MUST BE 0x100 ==> 0x80 items */
+	unsigned char *buf = (unsigned char *) b;
+
+	char *newbuf;
+	int newlen = 0;
+	int len;
+	int i, j;
+
+	len = strlen(b);
+
+	for (i = 0; i < len; newlen++) {
+		unsigned short discard;
+
+		i += ekg_utf8_helper(&buf[i], len - i, &discard);
+	}
+
+	newbuf = xmalloc(newlen+1);
+
+	for (i = 0, j = 0; buf[i]; j++) {
+		unsigned short znak;
+		int k;
+
+		i += ekg_utf8_helper(&buf[i], len - i, &znak);
+
+		if (znak < 0x80) {
+			newbuf[j] = znak;
+			continue;
+		}
+
+		newbuf[j] = '?';
+
+		for (k = 0; k < 0x80; k++) {
+			if (recode_table[k] == znak) {
+				newbuf[j] = (0x80 | k);
+				break;
+			}
+		}
+	}
+	newbuf[j] = '\0';
+
+	xfree(b);
+
+	return newbuf;
+}
+
+static char *ekg_to_utf8(char *b, const unsigned short *recode_table) {
+	unsigned char *buf = (unsigned char *) b;
+	char *newbuf;
+	int newlen = 0;
+	int i, j;
+
+	for (i = 0; buf[i]; i++) {
+		unsigned short znak = (buf[i] < 0x80) ? buf[i] : recode_table[buf[i]-0x80];
+
+		if (znak < 0x80)	newlen += 1;
+		else if (znak < 0x800)	newlen += 2;
+		else			newlen += 3;
+	}
+
+	newbuf = xmalloc(newlen+1);
+
+	for (i = 0, j = 0; buf[i]; i++) {
+		unsigned short znak = (buf[i] < 0x80) ? buf[i] : recode_table[buf[i]-0x80];
+		int count;
+
+		if (znak < 0x80)	count = 1;
+		else if (znak < 0x800)	count = 2;
+		else			count = 3;
+
+		switch (count) {
+			case 3: newbuf[j+2] = 0x80 | (znak & 0x3f); znak = znak >> 6; znak |= 0x800;
+			case 2: newbuf[j+1] = 0x80 | (znak & 0x3f); znak = znak >> 6; znak |= 0xc0;
+			case 1: newbuf[j] = znak;
+		}
+		j += count;
+	}
+	newbuf[j] = '\0';
+
+	xfree(b);
+	return newbuf;
+}
+
 /* cp1250 <==> any, use ekg_locale_to_cp() and ekg_cp_to_locale() */
 
 #if (USE_UNICODE || HAVE_GTK)
 static const unsigned short table_cp1250[] = {
-	0x20ac, '?', 0x201a, '?', 0x201e, 0x2026, 0x2020, 0x2021, 
-	'?', 0x2030, 0x0160, 0x2039, 0x015a, 0x0164, 0x017d, 0x0179, 
-	'?', 0x2018, 0x2019, 0x201c, 0x201d, 0x2022, 0x2013, 0x2014, 
-	'?', 0x2122, 0x0161, 0x203a, 0x015b, 0x0165, 0x017e, 0x017a, 
-	0x00a0, 0x02c7, 0x02d8, 0x0141, 0x00a4, 0x0104, 0x00a6, 0x00a7, 
-	0x00a8, 0x00a9, 0x015e, 0x00ab, 0x00ac, 0x00ad, 0x00ae, 0x017b, 
-	0x00b0, 0x00b1, 0x02db, 0x0142, 0x00b4, 0x00b5, 0x00b6, 0x00b7, 
-	0x00b8, 0x0105, 0x015f, 0x00bb, 0x013d, 0x02dd, 0x013e, 0x017c, 
-	0x0154, 0x00c1, 0x00c2, 0x0102, 0x00c4, 0x0139, 0x0106, 0x00c7, 
-	0x010c, 0x00c9, 0x0118, 0x00cb, 0x011a, 0x00cd, 0x00ce, 0x010e, 
-	0x0110, 0x0143, 0x0147, 0x00d3, 0x00d4, 0x0150, 0x00d6, 0x00d7, 
-	0x0158, 0x016e, 0x00da, 0x0170, 0x00dc, 0x00dd, 0x0162, 0x00df, 
-	0x0155, 0x00e1, 0x00e2, 0x0103, 0x00e4, 0x013a, 0x0107, 0x00e7, 
-	0x010d, 0x00e9, 0x0119, 0x00eb, 0x011b, 0x00ed, 0x00ee, 0x010f, 
-	0x0111, 0x0144, 0x0148, 0x00f3, 0x00f4, 0x0151, 0x00f6, 0x00f7, 
-	0x0159, 0x016f, 0x00fa, 0x0171, 0x00fc, 0x00fd, 0x0163, 0x02d9, 
+	0x20ac, '?', 0x201a, '?', 0x201e, 0x2026, 0x2020, 0x2021, 		/* 0x80 -      */
+	'?', 0x2030, 0x0160, 0x2039, 0x015a, 0x0164, 0x017d, 0x0179,		/*      - 0x8F */
+	'?', 0x2018, 0x2019, 0x201c, 0x201d, 0x2022, 0x2013, 0x2014,		/* 0x90 -      */ 
+	'?', 0x2122, 0x0161, 0x203a, 0x015b, 0x0165, 0x017e, 0x017a,		/*      - 0x9F */
+	0x00a0, 0x02c7, 0x02d8, 0x0141, 0x00a4, 0x0104, 0x00a6, 0x00a7,		/* 0xA0 -      */
+	0x00a8, 0x00a9, 0x015e, 0x00ab, 0x00ac, 0x00ad, 0x00ae, 0x017b, 	/*      - 0xAF */
+	0x00b0, 0x00b1, 0x02db, 0x0142, 0x00b4, 0x00b5, 0x00b6, 0x00b7,		/* 0xB0 -      */
+	0x00b8, 0x0105, 0x015f, 0x00bb, 0x013d, 0x02dd, 0x013e, 0x017c, 	/*      - 0xBF */
+	0x0154, 0x00c1, 0x00c2, 0x0102, 0x00c4, 0x0139, 0x0106, 0x00c7,		/* 0xC0 -      */
+	0x010c, 0x00c9, 0x0118, 0x00cb, 0x011a, 0x00cd, 0x00ce, 0x010e, 	/*      - 0xCF */
+	0x0110, 0x0143, 0x0147, 0x00d3, 0x00d4, 0x0150, 0x00d6, 0x00d7,		/* 0xD0 -      */
+	0x0158, 0x016e, 0x00da, 0x0170, 0x00dc, 0x00dd, 0x0162, 0x00df, 	/*      - 0xDF */
+	0x0155, 0x00e1, 0x00e2, 0x0103, 0x00e4, 0x013a, 0x0107, 0x00e7,		/* 0xE0 -      */
+	0x010d, 0x00e9, 0x0119, 0x00eb, 0x011b, 0x00ed, 0x00ee, 0x010f, 	/*      - 0xEF */
+	0x0111, 0x0144, 0x0148, 0x00f3, 0x00f4, 0x0151, 0x00f6, 0x00f7,		/* 0xF0 -      */
+	0x0159, 0x016f, 0x00fa, 0x0171, 0x00fc, 0x00fd, 0x0163, 0x02d9, 	/*      - 0xFF */
 };
 #endif
 
@@ -564,43 +646,6 @@ static char *gg_cp_to_iso(char *b) {
 	return b;
 }
 
-static char *gg_cp_to_utf8(char *b) {
-	unsigned char *buf = (unsigned char *) b;
-	char *newbuf;
-	int newlen = 0;
-	int i, j;
-
-	for (i = 0; buf[i]; i++) {
-		unsigned short znak = (buf[i] < 0x80) ? buf[i] : table_cp1250[buf[i]-0x80];
-
-		if (znak < 0x80)	newlen += 1;
-		else if (znak < 0x800)	newlen += 2;
-		else			newlen += 3;
-	}
-
-	newbuf = xmalloc(newlen+1);
-
-	for (i = 0, j = 0; buf[i]; i++) {
-		unsigned short znak = (buf[i] < 0x80) ? buf[i] : table_cp1250[buf[i]-0x80];
-		int count;
-
-		if (znak < 0x80)	count = 1;
-		else if (znak < 0x800)	count = 2;
-		else			count = 3;
-
-		switch (count) {
-			case 3: newbuf[j+2] = 0x80 | (znak & 0x3f); znak = znak >> 6; znak |= 0x800;
-			case 2: newbuf[j+1] = 0x80 | (znak & 0x3f); znak = znak >> 6; znak |= 0xc0;
-			case 1: newbuf[j] = znak;
-		}
-		j += count;
-	}
-	newbuf[j] = '\0';
-
-	xfree(b);
-	return newbuf;
-}
-
 static char *gg_iso_to_cp(char *b) {
 	unsigned char *buf = (unsigned char *) b;
 
@@ -615,60 +660,12 @@ static char *gg_iso_to_cp(char *b) {
 	return b;
 }
 
-static char *gg_utf8_to_cp(char *b) {
-	unsigned char *buf = (unsigned char *) b;
-
-	char *newbuf;
-	int newlen = 0;
-	int len;
-	int i, j;
-
-	if (!buf)
-		return NULL;
-
-	len = strlen(b);
-
-	for (i = 0; i < len; newlen++) {
-		unsigned short discard;
-
-		i += utf8_helper(&buf[i], len - i, &discard);
-	}
-
-	newbuf = xmalloc(newlen+1);
-
-	for (i = 0, j = 0; buf[i]; j++) {
-		unsigned short znak;
-		int k;
-
-		i += utf8_helper(&buf[i], len - i, &znak);
-
-		if (znak < 0x80) {
-			newbuf[j] = znak;
-			continue;
-		}
-
-		newbuf[j] = '?';
-
-		for (k = 0; k < (sizeof(table_cp1250)/sizeof(table_cp1250[0])); k++) {
-			if (table_cp1250[k] == znak) {
-				newbuf[j] = (0x80 | k);
-				break;
-			}
-		}
-	}
-	newbuf[j] = '\0';
-
-	xfree(b);
-
-	return newbuf;
-}
-
 char *ekg_locale_to_cp(char *buf) {
 	if (!buf)
 		return NULL;
 #if (USE_UNICODE || HAVE_GTK)
 	if (config_use_unicode)
-		return gg_utf8_to_cp(buf);
+		return ekg_from_utf8(buf, table_cp1250);
 #endif
 	if (cp_conv_out != EKG_ICONV_BAD)
 		return ekg_convert_string_p_safe(buf, cp_conv_out);
@@ -680,7 +677,7 @@ char *ekg_cp_to_locale(char *buf) {
 		return NULL;
 #if (USE_UNICODE || HAVE_GTK)
 	if (config_use_unicode)
-		return gg_cp_to_utf8(buf);
+		return ekg_to_utf8(buf, table_cp1250);
 #endif
 	if (cp_conv_in != EKG_ICONV_BAD) 
 		return ekg_convert_string_p_safe(buf, cp_conv_in);
@@ -713,103 +710,12 @@ static const unsigned short table_iso_8859_2[] = {
 void *latin2_conv_in = EKG_ICONV_BAD;
 void *latin2_conv_out = EKG_ICONV_BAD;
 
-static char *gg_utf8_to_latin2(char *b)
-{
-	unsigned char *buf = (unsigned char *) b;
-
-	char *newbuf;
-	int newlen = 0;
-	int len;
-	int i, j;
-
-	if (!buf)
-		return NULL;
-
-	len = strlen(b);
-
-	for (i = 0; i < len; newlen++) {
-		unsigned short discard;
-
-		i += utf8_helper(&buf[i], len - i, &discard);
-	}
-
-	newbuf = xmalloc(newlen+1);
-
-	for (i = 0, j = 0; buf[i]; j++) {
-		unsigned short znak;
-		int k;
-
-		i += utf8_helper(&buf[i], len - i, &znak);
-
-		if (znak < 0x80) {
-			newbuf[j] = znak;
-			continue;
-		}
-
-		newbuf[j] = '?';
-
-		for (k = 0; k < (sizeof(table_iso_8859_2)/sizeof(table_iso_8859_2[0])); k++) {
-			if (table_iso_8859_2[k] == znak) {
-				newbuf[j] = (0x80 | k);
-				break;
-			}
-		}
-	}
-	newbuf[j] = '\0';
-
-	xfree(buf);
-
-	return newbuf;
-}
-
-static char *gg_latin2_to_utf8(char *b)
-{
-	unsigned char *buf = (unsigned char *) b;
-	char *newbuf;
-	int newlen = 0;
-	int i, j;
-
-	if (!buf)
-		return NULL;
-
-	for (i = 0; buf[i]; i++) {
-		unsigned short znak = (buf[i] < 0x80) ? buf[i] : table_iso_8859_2[buf[i]-0x80];
-
-		if (znak < 0x80)	newlen += 1;
-		else if (znak < 0x800)	newlen += 2;
-		else			newlen += 3;
-	}
-
-	newbuf = xmalloc(newlen+1);
-
-	for (i = 0, j = 0; buf[i]; i++) {
-		unsigned short znak = (buf[i] < 0x80) ? buf[i] : table_iso_8859_2[buf[i]-0x80];
-		int count;
-
-		if (znak < 0x80)	count = 1;
-		else if (znak < 0x800)	count = 2;
-		else			count = 3;
-
-		switch (count) {
-			case 3: newbuf[j+2] = 0x80 | (znak & 0x3f); znak = znak >> 6; znak |= 0x800;
-			case 2: newbuf[j+1] = 0x80 | (znak & 0x3f); znak = znak >> 6; znak |= 0xc0;
-			case 1: newbuf[j] = znak;
-		}
-		j += count;
-	}
-	newbuf[j] = '\0';
-
-	xfree(buf);
-
-	return newbuf;
-}
-
 char *ekg_locale_to_latin2(char *buf) {
 	if (!buf)
 		return NULL;
 #if (USE_UNICODE || HAVE_GTK)
 	if (config_use_unicode)
-		return gg_utf8_to_latin2(buf);
+		return ekg_from_utf8(buf, table_iso_8859_2);
 #endif
 	if (latin2_conv_out != EKG_ICONV_BAD)
 		return ekg_convert_string_p_safe(buf, latin2_conv_out);
@@ -822,7 +728,7 @@ char *ekg_latin2_to_locale(char *buf) {
 		return NULL;
 #if (USE_UNICODE || HAVE_GTK)
 	if (config_use_unicode)
-		return gg_latin2_to_utf8(buf);
+		return ekg_to_utf8(buf, table_iso_8859_2);
 #endif
 	if (latin2_conv_in != EKG_ICONV_BAD)
 		return ekg_convert_string_p_safe(buf, latin2_conv_in);
