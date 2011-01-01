1c69e3569eec4622bc94b68b127896c486f941a1
Sławomir Nizio <poczta-sn@gazeta.pl>
2011-01-01 14:19:02 +0100

    fix for binding discard-line, yank didn't work OK

--- plugins/ncurses/bindings.c
+++ plugins/ncurses/bindings.c
@@ -298,7 +298,7 @@ static BINDING_FUNCTION(binding_accept_line)
 
 static BINDING_FUNCTION(binding_line_discard)
 {
-	if (ncurses_noecho) { /* we don't want to yank passwords */
+	if (!ncurses_noecho) { /* we don't want to yank passwords */
 		xfree(yanked);
 		yanked = xwcsdup(line);
 	}
@@ -317,7 +317,6 @@ static BINDING_FUNCTION(binding_line_discard)
 
 		lines_adjust();
 	}
-
 }
 
 static BINDING_FUNCTION(binding_quoted_insert)
