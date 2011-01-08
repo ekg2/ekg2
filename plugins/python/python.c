7bdcce7c725b1cda596ae43f9c000a7520d303d7
GiM913 <gim913@gmail.com>
2011-01-08 16:36:50 +0100

    during query changes I've forgot about python plugin
    query_name(self->id) -> self->name

--- plugins/python/python.c
+++ plugins/python/python.c
@@ -237,7 +237,7 @@ int python_query(script_t *scr, script_query_t *scr_que, void **args)
 				break;
 			}
 			default:
-			       debug("[NIMP] %s %d %d\n", __(query_name(scr_que->self->id)), i, scr_que->argv_type[i]);
+			       debug("[NIMP] %s %d %d\n", __(scr_que->self->name), i, scr_que->argv_type[i]);
 		}
 		if (!w) {
 			Py_INCREF(Py_None);
