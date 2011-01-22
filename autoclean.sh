bc5e902659b9c45e6de8ce80c0e8609dbcacfacd
Marcin Owsiany <marcin@owsiany.pl>
2011-01-22 17:52:33 +0000

    Added po/POTFILES.in to the source tree.
    We do not need to generate it in autogen.sh any more. This speeds up the build,
    and paves the way for using autoreconf.  The check_potfiles script run by "make
    check" will make sure we do not forget any source files containing translatable
    strings.

--- autoclean.sh
+++ autoclean.sh
@@ -9,7 +9,7 @@ rm -rf install-sh autom4te.cache config.guess config.sub libtool ltmain.sh stamp
 rm -rf compile libltdl
 rm -f plugins/perl/common/Makefile.old plugins/perl/irc/Makefile.old
 rm -f ABOUT-NLS
-rm -f po/Makefile.in.in po/Makevars.template po/POTFILES.in po/ekg2.pot
+rm -f po/Makefile.in.in po/Makevars.template po/ekg2.pot
 rm -f po/Rules-quot po/boldquot.sed po/quot.sed
 rm -f po/insert-header.sin po/remove-potcdate.sin
 rm -f po/en@boldquot.header po/en@quot.header
