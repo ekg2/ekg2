1ae272fb5615550d13042b5e19017a7f7ca33cd6
Marcin Owsiany <marcin@owsiany.pl>
2011-01-22 12:32:56 +0000

    Fix incorrect IRC variable initialization.
    Because the hardcoded variable offsets were wrong, the code was previously
    incorrectly setting "prefer_family" value to user's GECOS information, and left
    the "realname" variable uninitialized.
    
    Based on 46791e1086ad638bf2bfc571ba2ab7590849ab19 "Fix magic offsets for
    (nickname, realname) in irc" by darkjames.

--- plugins/irc/irc.c
+++ plugins/irc/irc.c
@@ -2352,10 +2352,12 @@ static plugins_params_t irc_plugin_vars[] = {
 	PLUGIN_VAR_ADD("identify",		VAR_STR, 0, 0, NULL),
 	PLUGIN_VAR_ADD("log_formats",		VAR_STR, "irssi", 0, NULL),
 	PLUGIN_VAR_ADD("make_window",		VAR_INT, "2", 0, NULL),
+#define IRC_PLUGIN_VAR_NICKNAME 20
 	PLUGIN_VAR_ADD("nickname",		VAR_STR, NULL, 0, NULL),		/* value will be inited @ irc_plugin_init() [pwd_entry->pw_name] */
 	PLUGIN_VAR_ADD("password",		VAR_STR, 0, 1, NULL),
 	PLUGIN_VAR_ADD("port",			VAR_INT, "6667", 0, NULL),
 	PLUGIN_VAR_ADD("prefer_family",		VAR_INT, "0", 0, NULL),
+#define IRC_PLUGIN_VAR_REALNAME 24
 	PLUGIN_VAR_ADD("realname",		VAR_STR, NULL, 0, NULL),		/* value will be inited @ irc_plugin_init() [pwd_entry->pw_gecos] */
 	PLUGIN_VAR_ADD("recode_list", VAR_STR, NULL, 0, irc_changed_recode_list),
 	PLUGIN_VAR_ADD("recode_out_default_charset", VAR_STR, NULL, 0, irc_changed_recode),		/* irssi-like-variable */
@@ -2422,9 +2424,9 @@ EXPORT int irc_plugin_init(int prio)
 	const char *pwd_realname = NULL;
 #endif
 
-/* magic stuff */
-	irc_plugin_vars[20].value = pwd_name;
-	irc_plugin_vars[23].value = pwd_realname;
+/* TODO: hardcoding offsets like this is fragile */
+	irc_plugin_vars[IRC_PLUGIN_VAR_NICKNAME].value = pwd_name;
+	irc_plugin_vars[IRC_PLUGIN_VAR_REALNAME].value = pwd_realname;
 
 	irc_plugin.params	= irc_plugin_vars;
 	irc_plugin.priv		= &irc_priv;
