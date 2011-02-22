e0da482d93211ee5a3c4c0dd4c8a8b5e1f5004d4
Michał Górny <mgorny@gentoo.org>
2011-02-22 20:42:06 +0100

    Rewrite completion match rewinding for utf8.

--- ekg/completion.c
+++ ekg/completion.c
@@ -1107,19 +1107,25 @@ exact_match:
 		}
 	
 		/* debug("common :%d\t\n", common); */
-#if UNICODE
 		{
-			char *p = completions[0];
-			wchar_t * wc = xmalloc((common+1) * sizeof(wchar_t));
-			if (mbsrtowcs(wc, &p, common, NULL) < 0)
-				common_len = 0;
-			else
-				common_len = p ? p - completions[0] : xstrlen(completions[0]);
-			xfree(wc);
+			/* that's the one _after_ last matching byte */
+			const gchar *p = &completions[0][common];
+
+			/* Check whether common didn't occur in a middle of a char
+			 * -> rewind to the previous char, then seek forward
+			 *
+			 * if we get the same offset, we got the whole char
+			 * if we get more, we partially matched something and need to dec
+			 */
+			common_len = common;
+			if (common > 0) {
+				const gchar *prev = g_utf8_prev_char(p);
+
+				if (g_utf8_next_char(prev) != p)
+					common_len -= (p - prev);
+			}
 		}
-#else
-		common_len = common;
-#endif
+
 		if (xstrlen(line) + common_len < line_maxlen) {
 			line[0] = '\0';
 			for(i = 0; i < words_count; i++) {
