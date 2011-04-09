3b15e6a68fd2af50ce3747502c8560429d22033f
Marcin Owsiany <marcin@owsiany.pl>
2011-04-09 16:53:23 +0100

    Make output directory tunable.

--- docs/generate-doc.sh
+++ docs/generate-doc.sh
@@ -4,6 +4,7 @@ rm -rf ./doxygen
 
 sed \
   -e "s,@OUTPUT_LANGUAGE@,Polish,g;" \
+  -e "s,@OUTPUT_DIRECTORY@,./doxygen,g;" \
   -e "s,@EXTRA_FILE_PATTERNS@,doxygenpl.txt,g;" \
   -e "s,@STRIP_FROM_PATH@,$(dirname ${PWD}),g" \
  doxygen.settings | doxygen -
