a0b0cc1a4818d195120455b336773519c8f181c8
Michał Górny <mgorny@gentoo.org>
2012-02-06 13:22:23 +0100

    Try to get plugin suffix from libtool and fallback to it.
    If it is possible to get plugin suffix from libtool after the configure,
    ekg2 will use that suffix to load plugins if it fails to find/load
    the .la file. This should help on systems with thoughtless .la removal
    mania.

--- ekg/plugins.c
+++ ekg/plugins.c
@@ -221,6 +221,34 @@ int plugin_load(const char *name, int prio, int quiet)
 	}
 
 	g_free(libname);
+
+#ifdef PLUGIN_SUFFIX
+	/* no .la found? try the standard suffix as well */
+	if (!plugin) {
+		libname = g_strdup_printf("%s" PLUGIN_SUFFIX, name);
+
+		if ((env_ekg_plugins_path = g_getenv("EKG_PLUGINS_PATH"))) {
+			lib = g_build_filename(env_ekg_plugins_path, libname, NULL);
+			plugin = ekg2_dlopen(lib);
+			g_free(lib);
+
+			if (!plugin) {
+				lib = g_build_filename(env_ekg_plugins_path, name, libname, NULL);
+				plugin = ekg2_dlopen(lib);
+				g_free(lib);
+			}
+		}
+
+		if (!plugin) {
+			lib = g_build_filename(PLUGINDIR, libname, NULL);
+			plugin = ekg2_dlopen(lib);
+			g_free(lib);
+		}
+
+		g_free(libname);
+	}
+#endif
+
 	/* prefer shared plugins */
 	if (plugin) {
 		init = g_strdup_printf("%s_plugin_init", name);
